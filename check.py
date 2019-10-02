861cdcd7ab12fbdc0d9485d400847b9921f6aae7
Passang Dorji <pd1813@vdi-n16.novalocal>
2019-10-02 16:15:18 +1000

    clean up of codes and fixing posix paths

--- check.py
+++ check.py
@@ -1,20 +1,28 @@
 #! /usr/bin/env python3
 
 import os
-import datetime
 from typing import List
 from pathlib import Path
 
 
-def _get_allowed_path_rows(path_row_file: Path) -> None:
-    with open(path_row_file, 'r') as fid:
-        return ['{:03}{:03}'.format(int(item.rstrip().split('_')[0]), int(item.rstrip().split('_')[1]))
-               for item in fid.readlines()]
+def _get_allowed_path_rows(path_row_file: Path) -> List:
+    with open(path_row_file, "r") as fid:
+        return [
+            "{:03}{:03}".format(
+                int(item.rstrip().split("_")[0]), int(item.rstrip().split("_")[1])
+            )
+            for item in fid.readlines()
+        ]
 
-def _check_level1_lists(level1_list: Path, check_codes: dict, pathrows_allowed: List) -> None:
 
-    with open(level1_list, 'r') as fid:
-        level1_list = [os.path.basename(item.rstrip()).split('_') for item in fid.readlines()]
+def _check_level1_lists(
+    level1_list: Path, check_codes: dict, pathrows_allowed: List
+) -> None:
+
+    with open(level1_list, "r") as fid:
+        level1_list = [
+            os.path.basename(item.rstrip()).split("_") for item in fid.readlines()
+        ]
 
     sensor = [item[0] for item in level1_list]
 
@@ -22,52 +30,43 @@ def _check_level1_lists(level1_list: Path, check_codes: dict, pathrows_allowed: 
     assert len(set(sensor)) == 1
 
     # check only allowed processing levels are in the list
-    if check_codes['sensor'] == 'L08':
-        assert set([item[1] for item in level1_list]) == set(check_codes['process_level'])
+    if check_codes["sensor"] == "L08":
+        assert set([item[1] for item in level1_list]) == set(
+            check_codes["process_level"]
+        )
 
     # check there no path/rows other than allowed list of path rows
     assert len(set([item[2] for item in level1_list]) - set(pathrows_allowed)) == 0
 
 
-def _gen_report(level1_list: List) -> None:
-   pass
-
-
 def main():
-    level1_ls8 = '/g/data1a/u46/users/pd1813/Collection_Upgrade/region_code_filter/L08_CollectionUpgrade_Level1_list.txt'
-    level1_ls7 = '/g/data1a/u46/users/pd1813/Collection_Upgrade/region_code_filter/L08_CollectionUpgrade_Level1_list.txt'
-    level1_ls5 = '/g/data1a/u46/users/pd1813/Collection_Upgrade/region_code_filter/L08_CollectionUpgrade_Level1_list.txt'
-
-    path_row_au = '/g/data1a/u46/users/pd1813/Collection_Upgrade/region_code_filter/Australian_Wrs_list.txt'
+    level1_ls8 = ("/g/data1a/u46/users/pd1813/Collection_Upgrade/region_code_filter/data/"
+                  "L08_CollectionUpgrade_Level1_list.txt")
+    level1_ls7 = ("/g/data1a/u46/users/pd1813/Collection_Upgrade/region_code_filter/data/"
+                  "L08_CollectionUpgrade_Level1_list.txt")
+    level1_ls5 = ("/g/data1a/u46/users/pd1813/Collection_Upgrade/region_code_filter/data/"
+                  "L08_CollectionUpgrade_Level1_list.txt")
+    path_row_au = ("/g/data1a/u46/users/pd1813/Collection_Upgrade/region_code_filter/data"
+                   "Australian_Wrs_list.txt")
     allowed_pathrows = _get_allowed_path_rows(Path(path_row_au))
 
     # check landsat8 level1 lists
     _check_level1_lists(
         Path(level1_ls8),
-        {'sensor': 'L08',
-         'process_level': ['L1TP', 'L1GT']
-        },
+        {"sensor": "L08", "process_level": ["L1TP", "L1GT"]},
         allowed_pathrows,
-     )
+    )
 
     # check landsat7 level1 lists
     _check_level1_lists(
-        Path(level1_ls7),
-        {'sensor': 'L07',
-         'process_level': ['L1TP']
-        },
-        allowed_pathrows,
-     )
+        Path(level1_ls7), {"sensor": "L07", "process_level": ["L1TP"]}, allowed_pathrows
+    )
 
     # check landsat5 level1 lists
     _check_level1_lists(
-        Path(level1_ls5),
-        {'sensor': 'L05',
-         'process_level': ['L1TP']
-        },
-        allowed_pathrows,
-     )
+        Path(level1_ls5), {"sensor": "L05", "process_level": ["L1TP"]}, allowed_pathrows
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- region_code_filter.py
+++ region_code_filter.py
@@ -4,28 +4,77 @@
 import os
 import logging
 from pathlib import Path
-from typing import Optional, List, Union
-import click
+from typing import List, Optional, Union
+import re
+import concurrent.futures
 
-import pandas as pd
+import click
 import geopandas as gpd
-import shapely.wkt
-from shapely.geometry import Polygon, MultiPolygon
+from shapely.geometry import Polygon
 from shapely.ops import cascaded_union
 
-
 _LOG = logging.getLogger(__name__)
+
+# landsat 8 filename pattern is configured to match only
+# processing level L1TP and L1GT for acquisition containing
+# both the TIRS and OLI sensors with .tar extension.
+L8_PATTERN = (
+    r"^(?P<sensor>LC)"
+    r"(?P<satellite>08)_"
+    r"(?P<processingCorrectionLevel>L1TP|L1GT)_"
+    r"(?P<wrsPath>[0-9]{3})"
+    r"(?P<wrsRow>[0-9]{3})_"
+    r"(?P<acquisitionDate>[0-9]{8})_"
+    r"(?P<processingDate>[0-9]{8})_"
+    r"(?P<collectionNumber>01)_"
+    r"(?P<collectionCategory>T1|T2)"
+    r"(?P<extension>.tar)$"
+)
+
+# landsat 8 filename pattern is configured to match only
+# processing level L1TP with .tar extension.
+L7_PATTERN = (
+    r"^(?P<sensor>LE)"
+    r"(?P<satellite>07)_"
+    r"(?P<processingCorrectionLevel>L1TP)_"
+    r"(?P<wrsPath>[0-9]{3})"
+    r"(?P<wrsRow>[0-9]{3})_"
+    r"(?P<acquisitionDate>[0-9]{8})_"
+    r"(?P<processingDate>[0-9]{8})_"
+    r"(?P<collectionNumber>01)_"
+    r"(?P<collectionCategory>T1|T2)"
+    r"(?P<extension>.tar)$"
+)
+
+# landsat 5 filename is configured to match only
+# processing level L1TP with .tar extension.
+L5_PATTERN = (
+    r"^(?P<sensor>LT)"
+    r"(?P<satellite>05)_"
+    r"(?P<processingCorrectionLevel>L1TP)_"
+    r"(?P<wrsPath>[0-9]{3})"
+    r"(?P<wrsRow>[0-9]{3})_"
+    r"(?P<acquisitionDate>[0-9]{8})_"
+    r"(?P<processingDate>[0-9]{8})_"
+    r"(?P<collectionNumber>01)_"
+    r"(?P<collectionCategory>T1|T2)"
+    r"(?P<extension>.tar)$"
+)
 
 
 def read_shapefile(shapefile: Path) -> gpd.GeoDataFrame:
     """ Code to read a shapefile and return its content as a geopandas dataframe"""
-    return gpd.read_file(shapefile)
+    return gpd.read_file(str(shapefile))
 
 
-def _get_auxiliary_extent(gpd_df: Path, subset_key: Optional[str] = None) -> Polygon:
+def _get_auxiliary_extent(
+    gpd_df: gpd.GeoDataFrame, subset_key: Optional[str] = None
+) -> Polygon:
     """ Returns the extent of all auxiliary dataset or an extent by a subset_key"""
     if subset_key:
-        return cascaded_union([geom for geom in gpd_df[gpd_df.auxiliary_ == subset_key].geometry])
+        return cascaded_union(
+            [geom for geom in gpd_df[gpd_df.auxiliary_ == subset_key].geometry]
+        )
     return cascaded_union([geom for geom in gpd_df.geometry])
 
 
@@ -39,21 +88,13 @@ def _auxiliary_overlap_extent(_extents: List[Polygon]) -> Polygon:
     return overlap_extent
 
 
-def path_row_filter(scene_to_filter_list, path_row_list: List[str]) -> None:
-    """Filter scenes to check if path/row of a scene is allowed in a path row list"""
-    for path_row in path_row_list:
-        print(path_row)
-
-
 def nbar_scene_filter(
-    nbar_auxiliary_extent: Polygon,
-    df_scenes_to_filter: Union[gpd.GeoDataFrame, Path]
+    nbar_auxiliary_extent: Polygon, df_scenes_to_filter: Union[gpd.GeoDataFrame, Path]
 ) -> List[str]:
     """ Filtering method to check if acquisition can be used for nbar processing"""
 
     if isinstance(df_scenes_to_filter, Path):
         df_scenes_to_filter = read_shapefile(df_scenes_to_filter)
-        print('yes')
 
     # initial filter is to check if scene intersects with auxiliary data extent
     aux_overlaped_gdf = gpd.GeoDataFrame()
@@ -61,88 +102,221 @@ def nbar_scene_filter(
         if geom.intersects(nbar_auxiliary_extent):
             aux_overlaped_gdf = aux_overlaped_gdf.append(df_scenes_to_filter.iloc[idx])
 
-    # aux_overlaped_gdf.to_file('/g/data/u46/users/pd1813/Collection_Upgrade/region_code_filter/test_outputs/overlapping_auxiliary_extent_no_aerosol.shp', driver="ESRI Shapefile")
-    #TODO additional check over ocean mask extent to see if scene is all offshore of inland
     return aux_overlaped_gdf
 
 
-def global_wrs_to_ga_extent(wrs_data: Path, aux_extents_vectorfiles: List[Path]) -> List[str]:
+def subset_global_tiles_to_ga_extent(
+    _global_tiles_data: Path,
+    aux_extents_vectorfiles: List[Path],
+    _satellite_data_provider: str,
+) -> List[str]:
     """ processing block for nbar scene filter"""
 
     # get extents from auxiliary vector files
-    extents = [_get_auxiliary_extent(read_shapefile(fp)) for fp in aux_extents_vectorfiles]
+    extents = [
+        _get_auxiliary_extent(read_shapefile(fp)) for fp in aux_extents_vectorfiles
+    ]
+    nbar_aux_extent = _auxiliary_overlap_extent(extents)
 
-    nbar_extent = _auxiliary_overlap_extent(extents)
-    # filter global wrs data
-    scenes_df = nbar_scene_filter(nbar_extent, wrs_data)
+    # filter global tile data to nbar_aux_extent
+    scenes_df = nbar_scene_filter(nbar_aux_extent, _global_tiles_data)
 
-    return list(scenes_df.PATH_ROW.values)
+    if _satellite_data_provider == "USGS":
+        return list(scenes_df.PATH_ROW.values)
+
+    return list(scenes_df.Name.values)
+
+
+def _write(filename: Path, list_to_write: List) -> None:
+    """ helper method to write contents in a list to a file"""
+    with open(filename, "w") as fid:
+        for item in list_to_write:
+            fid.write(item + "\n")
+
+
+def path_row_filter(
+    scenes_to_filter_list: Union[List[str], Path], path_row_list: Union[List[str], Path]
+) -> None:
+    """Filter scenes to check if path/row of a scene is allowed in a path row list"""
+
+    if isinstance(path_row_list, Path):
+        with open(path_row_list, "r") as fid:
+            path_row_list = [line.rstrip() for line in fid.readlines()]
+
+    path_row_list = [
+        "{:03}{:03}".format(int(item.split("_")[0]), int(item.split("_")[1]))
+        for item in path_row_list
+    ]
+    if isinstance(scenes_to_filter_list, Path):
+        with open(scenes_to_filter_list, "r") as fid:
+            scenes_to_filter_list = [line.rstrip() for line in fid.readlines()]
+
+    ls8_list, ls7_list, ls5_list = [], [], []
+
+    for scene_path in scenes_to_filter_list:
+        scene = os.path.basename(scene_path)
+
+        try:
+            path_row = scene.split("_")[2]
+        except IndexError:
+            _LOG.info(scene_path)
+            continue
+
+        if path_row not in path_row_list:
+            _LOG.info(scene_path)
+            continue
+
+        if re.match(L8_PATTERN, scene):
+            ls8_list.append(scene_path)
+
+        elif re.match(L7_PATTERN, scene):
+            ls7_list.append(scene_path)
+
+        elif re.match(L5_PATTERN, scene):
+            ls5_list.append(scene_path)
+
+        else:
+            _LOG.info(scene_path)
+
+    out_dir = Path.cwd()
+    _write(out_dir.joinpath("L08_CollectionUpgrade_Level1_list.txt"), ls8_list)
+    _write(out_dir.joinpath("L07_CollectionUpgrade_Level1_list.txt"), ls7_list)
+    _write(out_dir.joinpath("L05_CollectionUpgrade_Level1_list.txt"), ls5_list)
+
+
+def mgrs_filter(
+    scenes_to_filter_list: Union[List[str], Path], mgrs_list: Union[List[str], Path]
+) -> None:
+    """checks scenes to filter list if mrgs tile name are in mrgs list """
+    raise NotImplementedError
+
+
+def get_landsat_level1_file_paths(
+    nci_dir: Path, out_file: Path, nprocs: Optional[int] = 1
+) -> None:
+    """ Write all the files with *.tar in nci_dir to a text file"""
+
+    # this returns only folder name with PPP_RRR as is in NCI landsat archive
+    nci_path_row_dirs = [
+        nci_dir.joinpath(item)
+        for item in nci_dir.iterdir()
+        if re.match(r"[0-9]{3}_[0-9]{3}", item.name)
+    ]
+
+    # file paths searched using multiple threads
+    with open(out_file, "w") as fid:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=nprocs) as executor:
+            results = [
+                executor.submit(
+                    lambda x: [fp.as_posix() for fp in x.glob("**/*.tar")], path_row
+                )
+                for path_row in nci_path_row_dirs
+            ]
+            for pt_list in concurrent.futures.as_completed(results):
+                for _fp in pt_list.result():
+                    fid.write(_fp + "\n")
 
 
 @click.command()
 @click.option(
-    '--brdf-shapefile',
+    "--brdf-shapefile",
     type=click.Path(dir_okay=False, file_okay=True),
-    help='full path to brdf extent shapefile',
-    default='/g/data/u46/users/pd1813/Collection_Upgrade/region_code_filter/auxiliary-extents/brdf_tiles_new.shp'
+    help="full path to brdf extent shapefile",
+    default="/g/data/u46/users/pd1813/Collection_Upgrade/region_code_filter/auxiliary-extents/brdf_tiles_new.shp",
 )
 @click.option(
-    '--one-deg-dsm-v1-shapefile',
+    "--one-deg-dsm-v1-shapefile",
     type=click.Path(dir_okay=False, file_okay=True),
-    help='full path to one deg dsm version 1 extent shapefile',
-    default='/g/data/u46/users/pd1813/Collection_Upgrade/region_code_filter/auxiliary-extents/one-deg-dsm-v1.shp'
-)
-
-@click.option(
-    '--one-sec-dsm-v1-shapefile',
-    type=click.Path(dir_okay=False, file_okay=True),
-    help='full path to one sec dsm version 1 shapefile',
-    default='/g/data/u46/users/pd1813/Collection_Upgrade/region_code_filter/auxiliary-extents/one-sec-dsm-v1.shp'
+    help="full path to one deg dsm version 1 extent shapefile",
+    default="/g/data/u46/users/pd1813/Collection_Upgrade/region_code_filter/auxiliary-extents/one-deg-dsm-v1.shp",
 )
 @click.option(
-    '--one-deg-dsm-v2-shapefile',
-    type=click.Path(dir_okay=False, file_okay=True),
-    help='full path to dsm shapefile',
-    default='/g/data/u46/users/pd1813/Collection_Upgrade/region_code_filter/auxiliary-extents/one-deg-dsm-v2.shp'
+    "--one-sec-dsm-v1-shapefile",
+    type=click.Path(dir_okay=False, file_okay=True, exists=True),
+    help="full path to one sec dsm version 1 shapefile",
+    default="/g/data/u46/users/pd1813/Collection_Upgrade/region_code_filter/auxiliary-extents/one-sec-dsm-v1.shp",
 )
 @click.option(
-    '--aerosol-shapefile',
-    type=click.Path(dir_okay=False, file_okay=True),
-    help='full path to aerosol shapefile',
-    default='/g/data/u46/users/pd1813/Collection_Upgrade/region_code_filter/auxiliary-extents/aerosol.shp'
+    "--one-deg-dsm-v2-shapefile",
+    type=click.Path(dir_okay=False, file_okay=True, exists=True),
+    help="full path to dsm shapefile",
+    default="/g/data/u46/users/pd1813/Collection_Upgrade/region_code_filter/auxiliary-extents/one-deg-dsm-v2.shp",
 )
 @click.option(
-    '--world-wrs-shapefile',
-    type=click.Path(dir_okay=False, file_okay=True),
-    help='full path to global wrs shapefile',
-    default='/g/data/u46/users/pd1813/Collection_Upgrade/region_code_filter/global_wrs_mgrs_shps/wrsdall_Decending.shp'
+    "--aerosol-shapefile",
+    type=click.Path(dir_okay=False, file_okay=True, exists=True),
+    help="full path to aerosol shapefile",
+    default="/g/data/u46/users/pd1813/Collection_Upgrade/region_code_filter/auxiliary-extents/aerosol.shp",
 )
 @click.option(
-    '--world-mgrs-shapefile',
+    "--satellite-data-provider",
+    type=click.Choice(["ESA", "USGS"]),
+    help="satellite data provider (ESA or USGS)",
+    default="USGS",
+)
+@click.option(
+    "--world-wrs-shapefile",
+    type=click.Path(dir_okay=False, file_okay=True, exists=True),
+    help="full path to global wrs shapefile",
+    default="/g/data/u46/users/pd1813/Collection_Upgrade/region_code_filter/global_wrs_mgrs_shps/wrsdall_Decending.shp",
+)
+@click.option(
+    "--world-mgrs-shapefile",
+    type=click.Path(dir_okay=False, file_okay=True, exists=True),
+    help="full path to global mgrs shapefile",
+    default="/g/data/u46/users/pd1813/Collection_Upgrade/region_code_filter/global_wrs_mgrs_shps/S2_tile.shp",
+)
+@click.option(
+    "--usgs-level1-files",
     type=click.Path(dir_okay=False, file_okay=True),
-    help='full path to global mgrs shapefile',
-    default='/g/data/u46/users/pd1813/Collection_Upgrade/region_code_filter/global_wrs_mgrs_shps/S2_tile.shp'
+    help="full path to a text files containing all the level-1 usgs/esa  list to be filtered",
+)
+@click.option(
+    "--allowed-codes",
+    type=click.Path(dir_okay=False, file_okay=True, exists=True),
+    help="full path to a text files containing path/row or MGRS tile name to act as a filter",
 )
 def main(
     brdf_shapefile: click.Path,
     one_deg_dsm_v1_shapefile: click.Path,
     one_sec_dsm_v1_shapefile: click.Path,
     one_deg_dsm_v2_shapefile: click.Path,
+    satellite_data_provider: str,
     aerosol_shapefile: click.Path,
     world_wrs_shapefile: click.Path,
-    world_mgrs_shapefile: click.Path
+    world_mgrs_shapefile: click.Path,
+    usgs_level1_files: click.Path,
+    allowed_codes: click.Path,
 ):
-    wrs_list = global_wrs_to_ga_extent(Path(world_wrs_shapefile),
-                                       [brdf_shapefile,
-                                       one_deg_dsm_v1_shapefile,
-                                       one_sec_dsm_v1_shapefile,
-                                       one_deg_dsm_v2_shapefile
-                                       ]
-               )
+    if not usgs_level1_files:
+        get_landsat_level1_file_paths(
+            Path("/g/data/da82/AODH/USGS/L1/Landsat/C1"),
+            Path(
+                "/g/data/u46/users/pd1813/Collection_Upgrade/region_code_filter/all_landsat_scenes.txt"
+            ),
+            nprocs=8,
+        )
 
-    #TODO write level1 yaml file to filter the scenes to wrs in the wrs list
-    print(wrs_list)
+    if not allowed_codes:
+        _extent_list = [
+            brdf_shapefile,
+            one_deg_dsm_v1_shapefile,
+            one_sec_dsm_v1_shapefile,
+            one_deg_dsm_v2_shapefile,
+        ]
+        global_tiles_data = Path(world_wrs_shapefile)
+        if satellite_data_provider == "ESA":
+            global_tiles_data = Path(world_mgrs_shapefile)
+        allowed_codes = subset_global_tiles_to_ga_extent(
+            global_tiles_data, _extent_list, satellite_data_provider
+        )
 
-if __name__ == '__main__':
+    path_row_filter(
+        Path(usgs_level1_files),
+        Path(allowed_codes) if isinstance(allowed_codes, str) else allowed_codes,
+    )
+
+
+if __name__ == "__main__":
+    logging.basicConfig(filename="ignored_scenes_list.log", level=logging.INFO)
     main()
-
