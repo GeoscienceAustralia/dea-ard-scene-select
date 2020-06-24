f0fa71504b793e794638e89f11a775266d3dc4d0
Duncan Gray <duncan.gray@ga.gov.au>
2020-06-24 14:54:17 +1000

    added script to kick off production jobs

---
+++ ard_production.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# region code needs datacube.
+#source /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env
+module use /g/data/v10/public/modules/modulefiles
+module load dea
+
+# prod example
+python3 region_code_filter.py --products '["ga_ls5t_level1_3"]' --workdir /g/data/v10/projects/landsat_c3/wagl_workdir  --pkgdir  /g/data/xu18/ga --logdir /g/data/v10/projects/landsat_c3/wagl_logdir --env /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env --project v10 --walltime 05:00:00 #--run-ard
