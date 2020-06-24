87f1bcef2b14278e5cd3849a9a1ea313cb4a106a
Duncan Gray <duncan.gray@ga.gov.au>
2020-06-24 17:06:18 +1000

    do ard processing on 4 products - ga ls7 job was done before

--- ard_production.sh
+++ ard_production.sh
@@ -6,4 +6,4 @@ module use /g/data/v10/public/modules/modulefiles
 module load dea
 
 # prod example
-python3 region_code_filter.py --products '["ga_ls7e_level1_3"]' --workdir /g/data/v10/projects/landsat_c3/wagl_workdir  --pkgdir  /g/data/xu18/ga --logdir /g/data/v10/projects/landsat_c3/wagl_logdir --env /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env --project v10 --walltime 05:00:00 #--run-ard
+python3 region_code_filter.py --products '["ga_ls5t_level1_3", "usgs_ls5t_level1_1", "usgs_ls7e_level1_1", "usgs_ls8c_level1_1"]' --workdir /g/data/v10/projects/landsat_c3/wagl_workdir  --pkgdir  /g/data/xu18/ga --logdir /g/data/v10/projects/landsat_c3/wagl_logdir --env /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env --project v10 --walltime 05:00:00 --run-ard
