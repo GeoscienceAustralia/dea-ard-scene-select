ed68063165e35132b73056ef3044e5ca697f2e56
Duncan Gray <duncan.gray@ga.gov.au>
2020-06-26 09:40:57 +1000

    update the test script

--- examples.sh
+++ examples.sh
@@ -6,6 +6,9 @@ module load dea
 # production example
 #python3 region_code_filter.py --products '["ga_ls5t_level1_3"]' --workdir /g/data/v10/projects/landsat_c3/wagl_workdir  --pkgdir  /g/data/xu18/ga --logdir /g/data/v10/projects/landsat_c3/wagl_logdir --env /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env --project v10 --walltime 05:00:00 #--run-ard
 
+#  local work dir
+python3 region_code_filter.py --workdir scratch/  --pkgdir  scratch/ --logdir scratch/ --env /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env --project v10 --walltime 05:00:00 --landsat-AOI #--run-ard
+
 # skipping odc
 #python3 region_code_filter.py  --usgs-level1-files data/small_Landsat_Level1_Nci_Files.txt
 
