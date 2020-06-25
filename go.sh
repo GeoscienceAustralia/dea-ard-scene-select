4daff332fc9fb9e358ae26397bf157d2487131ad
Duncan Gray <duncan.gray@ga.gov.au>
2020-06-25 16:23:56 +1000

    cleaning up go.sh

--- go.sh
+++ go.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 
-# region code needs datacube.
-#source /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env
 module use /g/data/v10/public/modules/modulefiles
 module load dea
 
-# prod example
+# production example
 #python3 region_code_filter.py --products '["ga_ls5t_level1_3"]' --workdir /g/data/v10/projects/landsat_c3/wagl_workdir  --pkgdir  /g/data/xu18/ga --logdir /g/data/v10/projects/landsat_c3/wagl_logdir --env /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env --project v10 --walltime 05:00:00 #--run-ard
 
-#python3 region_code_filter.py --products '["ga_ls5t_level1_3", "usgs_ls5t_level1_1", "usgs_ls7e_level1_1", "usgs_ls8c_level1_1"]' --workdir $PWD  --pkgdir $PWD --logdir $PWD --env /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env --project v10 --walltime 05:00:00 #--run-ard
-
 # skipping odc
-python3 region_code_filter.py  --usgs-level1-files data/small_Landsat_Level1_Nci_Files.txt --workdir  scratch/ --pkgdir scratch/ --logdir scratch/  #--run-ard
+python3 region_code_filter.py  --usgs-level1-files data/small_Landsat_Level1_Nci_Files.txt --workdir  scratch/ --pkgdir scratch/ --logdir scratch/
 
 # testing example
 #python3 region_code_filter.py --products '["ga_ls5t_level1_3"]' --workdir $PWD  --pkgdir $PWD --logdir $PWD --env /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env --project v10 --walltime 05:00:00 #--run-ard
-
-#python3 region_code_filter.py --products '["ga_ls5t_level1_3"]' --workdir $PWD  --pkgdir $PWD --logdir $PWD --env /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env --nodes 1 --project v10 --walltime 00:10:00 --test --run-ard
-
-#python3 region_code_filter.py --products '[]' --workdir $PWD --logdir $PWD --pkgdir $PWD --env /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env --nodes 1 --project v10 --walltime 00:10:00 --test
-
-
-#python3 region_code_filter.py --products '["ga_ls5t_level1_3"]' --workdir $PWD --logdir $PWD --pkgdir $PWD --env /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env --nodes 1 --project v10 --walltime 00:10:00 --test
-#python3 region_code_filter.py --workdir $PWD --logdir $PWD --pkgdir $PWD --env /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env --nodes 1 --project v10 --walltime 05:00:00
