487f451acc3e0f333f98a9beb49ad24feb2e9913
Duncan Gray <duncan.gray@ga.gov.au>
2020-06-26 22:27:42 +1000

    tinkering

--- examples.sh
+++ examples.sh
@@ -16,7 +16,7 @@ python3 region_code_filter.py --workdir scratch/  --pkgdir  scratch/ --logdir sc
 #python3 region_code_filter.py  --usgs-level1-files data/All_Landsat_Level1_Nci_Files.txt  --workdir scratch/ --allowed-codes data/Australian_Wrs_list.txt
 
 # using Landsat AOI
-python3 region_code_filter.py  --usgs-level1-files data/All_Landsat_Level1_Nci_Files.txt  --workdir scratch/ --landsat-AOI
+#python3 region_code_filter.py  --usgs-level1-files data/All_Landsat_Level1_Nci_Files.txt  --workdir scratch/ --landsat-AOI
 
 # testing example
 #python3 region_code_filter.py --products '["ga_ls5t_level1_3"]' --workdir $PWD  --pkgdir $PWD --logdir $PWD --env /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env --project v10 --walltime 05:00:00 #--run-ard
