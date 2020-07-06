85e79f31e0b36717b27ad4ca6abb4ab38a901222
Duncan Gray <duncan.gray@ga.gov.au>
2020-07-07 08:34:55 +1000

    Move data used in the example code. Remove examples dir from nci module

--- examples/dsg_examples.sh
+++ examples/dsg_examples.sh
@@ -5,7 +5,7 @@ module use /home/547/dsg547/devmodules/modulefiles
 module load ard-scene-select-py3-dea/20200706
 
 # local work dir and skipping odc
-ard-scene-select --usgs-level1-files data/small_Landsat_Level1_Nci_Files.txt --workdir scratch/ 
+ard-scene-select --usgs-level1-files small_Landsat_Level1_Nci_Files.txt --workdir scratch/
 
 # local work dir 
 ard-scene-select --products '["ga_ls5t_level1_3"]' --workdir scratch/ 
