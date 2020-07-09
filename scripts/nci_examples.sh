ba1a2cacdb5636907739d53b9b02acc8b2ada5fd
Duncan Gray <duncan.gray@ga.gov.au>
2020-07-09 12:11:00 +1000

    setting up more for production.

---
+++ scripts/nci_examples.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#PBS -P u46
+#PBS -W umask=017
+#PBS -q express
+#PBS -l walltime=00:30:00,mem=155GB,other=pernodejobfs
+#PBS -l wd
+#PBS -l storage=gdata/v10+scratch/v10+gdata/if87+gdata/fj7+scratch/fj7+scratch/u46+gdata/u46
+#PBS -l ncpus=1
+
+module use /g/data/v10/public/modules/modulefiles
+module use /g/data/v10/private/modules/modulefiles
+
+module load ard-scene-select-py3-dea/20200707
+
+# local work dir 
+ard-scene-select --workdir scratch/
+
+# local work dir and skipping odc
+#ard-scene-select --usgs-level1-files small_Landsat_Level1_Nci_Files.txt --workdir scratch/
+
+# local work dir 
+#ard-scene-select --products '["ga_ls5t_level1_3"]' --workdir scratch/ 
+
+# need access to v10 for this to work
+# ard-scene-select --workdir /g/data/v10/projects/landsat_c3/wagl_workdir  --pkgdir  /g/data/xu18/ga --logdir /g/data/v10/projects/landsat_c3/wagl_logdir --env /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env --project v10 --walltime 05:00:00 #--run-ard
---
+++ scripts/test_ard_production.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#PBS -P v10
+#PBS -W umask=017
+#PBS -q normal
+#PBS -l walltime=0:30:00,mem=15GB,other=pernodejobfs
+#PBS -l wd
+#PBS -l storage=gdata/v10+scratch/v10+gdata/if87+gdata/fj7+scratch/fj7+scratch/u46+gdata/u46
+#PBS -l ncpus=1
+
+# Note it is assumed the user lpgs will be executing this script with;
+# qsub ard_production
+
+module use /g/data/v10/public/modules/modulefiles
+module use /g/data/v10/private/modules/modulefiles
+
+module load ard-scene-select-py3-dea/20200707
+
+# local work dir and skipping odc
+ard-scene-select --workdir /g/data/v10/projects/landsat_c3/wagl_workdir  --pkgdir  /g/data/xu18/ga --logdir /g/data/v10/projects/landsat_c3/wagl_logdir --env /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env --project v10 --walltime 05:00:00 #--run-ard
+
+# This flag is handy for doing a test and you want a result quickly
+#  --products '["ga_ls5t_level1_3"]'
