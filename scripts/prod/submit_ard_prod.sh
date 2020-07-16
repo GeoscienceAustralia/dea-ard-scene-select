3a07420ad75d65be2183488c4b9cea66c2af5124
Duncan Gray <duncan.gray@ga.gov.au>
2020-07-16 16:59:24 +1000

    still fixing

--- scripts/prod/submit_ard_prod.sh
+++ scripts/prod/submit_ard_prod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Capturing the .e & .o files in a run dir
-$INIT_PWD=$PWD
+INIT_PWD=$PWD
 RUN_DIR='/g/data/v10/work/c3_ard/logdir/jobdir'
 cd $RUN_DIR
 qsub $INIT_PWD/job_ard_prod.sh
