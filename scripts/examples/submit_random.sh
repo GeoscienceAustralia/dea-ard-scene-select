2056ee8b0a99660affca7f18404e8c70f0f1634d
Duncan Gray <duncan.gray@ga.gov.au>
2020-07-20 12:03:30 +1000

    redirecting to log files

--- scripts/examples/submit_random.sh
+++ scripts/examples/submit_random.sh
@@ -5,4 +5,4 @@
 INIT_PWD=$PWD
 RUN_DIR='/g/data/v10/projects/c3_ard/test'
 cd $RUN_DIR
-qsub -v INIT_PWD=$INIT_PWD $INIT_PWD/random.sh
+qsub -v INIT_PWD=$INIT_PWD $INIT_PWD/random.sh >  $RUN_DIR/submit_ard_prod.log 2>$RUN_DIR/submit_ard_prod..err
