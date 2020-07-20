f0ba367a1392d1cbe5283e468e568edf7b9a269f
Duncan Gray <duncan.gray@ga.gov.au>
2020-07-20 14:06:34 +1000

    debugging

--- scripts/examples/submit_random.sh
+++ scripts/examples/submit_random.sh
@@ -2,9 +2,10 @@
 
 # ssh lpgs@gadi-dm.nci.org.au /g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/examples/submit_random.sh
 # Capturing the .e & .o files in a run dir
-INIT_PWD=$PWD
+CODE_DIR='/g/data/u46/users/dsg547/sandpit/dea-ard-scene-select/scripts/prod'
+CODE_DIR='/g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/examples'
 RUN_DIR='/g/data/v10/projects/c3_ard/test'
 #RUN_DIR='/g/data/u46/users/dsg547/sandpit/dea-ard-scene-select/scripts/examples/scratch'
 cd $RUN_DIR
 PBS_LOG=$RUN_DIR/submit_ard_prod.log
-qsub -v INIT_PWD=$INIT_PWD $INIT_PWD/random.sh >>$PBS_LOG 2>&1
+qsub -v INIT_PWD=$CODE_DIR $CODE_DIR/random.sh >>$PBS_LOG 2>&1
