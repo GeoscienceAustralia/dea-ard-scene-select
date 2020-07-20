a1d3ac35218361478937dd69856d45bcb9030dd8
Duncan Gray <duncan.gray@ga.gov.au>
2020-07-20 15:20:45 +1000

    comments

--- scripts/examples/submit_random.sh
+++ scripts/examples/submit_random.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# This will work on gadi
 # ssh lpgs@gadi-dm.nci.org.au /g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/examples/submit_random.sh
+
+# This will not work on gadi
+# But should work everywehere else
+#ssh lpgs@gadi.nci.org.au /g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/examples/submit_random.sh
+
 # Capturing the .e & .o files in a run dir
 CODE_DIR='/g/data/u46/users/dsg547/sandpit/dea-ard-scene-select/scripts/prod'
 CODE_DIR='/g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/examples'
