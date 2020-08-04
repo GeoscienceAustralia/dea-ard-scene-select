916550881eb2340ac0272d0ad0fc2890efa320f9
Duncan Gray <duncan.gray@ga.gov.au>
2020-08-04 14:42:21 +1000

    adding comments

--- run_black.sh
+++ run_black.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Convenience script for running black
+# potentially get this going in .pre-commit-config.yaml
 module use /g/data/v10/public/modules/modulefiles
 module load dea
 
