4d9301ade900559c2a7bdffa7114da81b6778b7a
Duncan Gray <duncan.gray@ga.gov.au>
2020-08-18 16:49:31 +1000

    change dir structure. add files testing airflow type logic

---
+++ tests/scripts/airflow/qsub_ls.sh
@@ -0,0 +1,18 @@
+WORK_DIR='/home/547/dsg547/dump/airflow/qsub_ls_results'
+
+module use /g/data/v10/public/modules/modulefiles;
+module load dea;
+
+mkdir -p $WORK_DIR
+cd $WORK_DIR
+
+qsub -N qsub_ls \
+              -q express \
+              -W umask=33 \
+              -l wd,walltime=0:01:00,mem=1GB,ncpus=1 -m abe \
+              -l storage=gdata/v10+gdata/u46+gdata/fk4+gdata/rs0+gdata/if87 \
+              -P u46 -o $WORK_DIR -e $WORK_DIR \
+              -- /bin/bash -l -c \
+                  "module use /g/data/v10/public/modules/modulefiles/; \
+                  module load dea; \
+                  datacube --version"
