6ed010a6aa2b371d0d0ec42caf8187b9dcf65bf3
Duncan Gray <duncan.gray@ga.gov.au>
2020-08-27 15:52:19 +1000

    logging testing

--- tests/scripts/logging/basic.py
+++ tests/scripts/logging/basic.py
@@ -1,17 +1,9 @@
-import logs
+from dass_logs import LOGGER
 from logging.config import fileConfig
 
-fileConfig('logging.cfg')
+fileConfig('basic_config.ini')
 
-# INTERFACE_LOGGER luigi-interface.log
-# 2020-08-26 17:20:39,131: INFO: yeah
+LOGGER.info('moo cow', state='ACT')
 
-# STATUS_LOGGER status.log
-# {"event": "zoo", "level": "info", "timestamp": "2020-08-26T07:23:04.788129Z"}
 
-logs.INTERFACE_LOGGER.info('yeah')
-
-logs.STATUS_LOGGER.info('zoo', state='yo')
-#{"event": "zoo", "level": "info", "state": "yo", "timestamp": "2020-08-27T.."}
-
-logs.TASK_LOGGER.info('flying', state='yo')
+LOGGER.info('scene removed', datawet_id='bsrflkewjr', reason="scene id in ARD")
