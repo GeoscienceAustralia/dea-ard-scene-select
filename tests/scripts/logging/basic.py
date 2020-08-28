0aceea51bea8ef40505a0792c04f2c29285362c3
Duncan Gray <duncan.gray@ga.gov.au>
2020-08-28 13:13:04 +1000

    experimenting with logging

--- tests/scripts/logging/basic.py
+++ tests/scripts/logging/basic.py
@@ -1,7 +1,12 @@
 from dass_logs import LOGGER
 from logging.config import fileConfig
+from pathlib import Path
 
-fileConfig('basic_config.ini')
+the_logfilename = Path(__file__).parent.joinpath("basic.log").resolve()
+print (the_logfilename)
+fileConfig('basic_config.ini' , disable_existing_loggers=False, defaults={ 'logfilename' :  '/g/data/u46/users/dsg547/sandpit/dea-ard-scene-select/tests/scripts/logging/basic.log'} )
+#fileConfig('basic_config.ini' , disable_existing_loggers=False, defaults={ 'logfilename' :  str(the_logfilename)} )
+#fileConfig('basic_config.ini' , disable_existing_loggers=False, defaults={ 'logfilename' :  '/g/data/u46/users/dsg547/sandpit/dea-ard-scene-select/tests/scripts/logging/scratch/logals.txt'} )
 
 LOGGER.info('moo cow', state='ACT')
 
