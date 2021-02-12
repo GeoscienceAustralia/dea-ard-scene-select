99535f97e71174b8d4b97b965859dfd0d86b5a61
Duncan Gray <duncan.gray@ga.gov.au>
2021-02-12 11:08:53 +1100

    Hacky way of separating rsync and update

--- scripts/reprocessing/step1.sh
+++ scripts/reprocessing/step1.sh
@@ -2,8 +2,8 @@
 module use /g/data/v10/public/modules/modulefiles
 module load dea
 
-#is_test=0 # True # test
-is_test=1 # False # production
+is_test=0 # True # test
+#is_test=1 # False # production
 if [ $is_test = 0 ] ; then
     old_base='/g/data/u46/users/dsg547/test_data/c3/reprocessing/ard_standard/'
     new_base='/g/data/u46/users/dsg547/test_data/c3/reprocessing/ard_new/'
@@ -20,9 +20,11 @@ echo $old_path
 echo $new_path
 mkdir -p $new_path
 
-rsync -av $old_path $new_path
-
-# Update needs documentation...
-newyaml="$new_base$1"
-echo $newyaml
-datacube --config dsg547_dev.conf dataset update $newyaml  --location-policy forget #  --dry-run
+a_cmd="rsync"
+if [ $a_cmd = 0 ] ; then
+    rsync -av $old_path $new_path
+else
+    newyaml="$new_base$1"
+    echo $newyaml
+    datacube --config dsg547_dev.conf dataset update $newyaml  --location-policy forget #  --dry-run
+fi
