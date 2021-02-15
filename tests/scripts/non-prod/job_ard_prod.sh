1c769c526408e78c452d032cbbd0b84921dd3717
Duncan Gray <duncan.gray@ga.gov.au>
2021-02-16 10:04:45 +1100

    testing ard-pipelines

--- tests/scripts/non-prod/job_ard_prod.sh
+++ tests/scripts/non-prod/job_ard_prod.sh
@@ -15,7 +15,8 @@ module use /g/data/v10/public/modules/modulefiles
 module use /g/data/v10/private/modules/modulefiles
 #module use /home/547/dsg547/devmodules/modulefiles
 
-module load ard-scene-select-py3-dea/20201126
+#module load ard-scene-select-py3-dea/20201126
+module load ard-scene-select-py3-dea/20210215
 
 [[ ":$PYTHONPATH:" != *":$SSPATH:"* ]] && PYTHONPATH="$SSPATH:${PYTHONPATH}"
 
@@ -23,10 +24,13 @@ module load ard-scene-select-py3-dea/20201126
 INIT_PWD=$PWD
 
 # local dev
-python3 ../../../scene_select/ard_scene_select.py --products '["usgs_ls7e_level1_1"]' --workdir $INIT_PWD/scratch/ --pkgdir $INIT_PWD/scratch/ --logdir $INIT_PWD/scratch/ --env $INIT_PWD/prod-wagl.env --index-datacube-env $INIT_PWD/dsg547_dev.env  --project u46 --walltime 04:00:00  --config $INIT_PWD/dsg547_dev.conf --find-blocked # --products '["usgs_ls8c_level1_1"]'
+#python3 ../../../scene_select/ard_scene_select.py --products '["usgs_ls7e_level1_1"]' --workdir $INIT_PWD/scratch/ --pkgdir $INIT_PWD/scratch/ --logdir $INIT_PWD/scratch/ --env $INIT_PWD/prod-wagl.env --index-datacube-env $INIT_PWD/dsg547_dev.env  --project u46 --walltime 04:00:00  --config $INIT_PWD/dsg547_dev.conf --find-blocked # --products '["usgs_ls8c_level1_1"]'
 
 # A module
-#ard-scene-select --products '["usgs_ls8c_level1_1"]' --workdir $INIT_PWD/scratch/ --pkgdir $INIT_PWD/scratch/ --logdir $INIT_PWD/scratch/ --env $INIT_PWD/prod-wagl.env --index-datacube-env $INIT_PWD/dsg547_dev.env  --project u46 --walltime 01:00:00  --config $INIT_PWD/dsg547_dev.conf  # --run-ard # note will try and index
+ard-scene-select --products '["usgs_ls7e_level1_1"]' --workdir $INIT_PWD/scratch/ --pkgdir $INIT_PWD/scratch/ --logdir $INIT_PWD/scratch/ --env $INIT_PWD/prod-20201030.env --index-datacube-env $INIT_PWD/dsg547_dev.env  --project u46 --walltime 10:00:00  #--config $INIT_PWD/dsg547_dev.conf  # --run-ard # note will try and index
 
 # to use the dev db
 #  --config $INIT_PWD/dsg547_dev.conf --products '["usgs_ls8c_level1_1"]'
+
+#PRODUCTS = '["ga_ls5t_level1_3", "ga_ls7e_level1_3", \
+    #     "usgs_ls5t_level1_1", "usgs_ls7e_level1_1", "usgs_ls8c_level1_1"]'
