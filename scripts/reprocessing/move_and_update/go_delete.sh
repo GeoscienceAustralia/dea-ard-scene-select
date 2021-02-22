e62a4c65e256d442e0a4bd3fc25399b18753a44f
Duncan Gray <duncan.gray@ga.gov.au>
2021-02-22 14:09:35 +1100

    fix typo

--- scripts/reprocessing/move_and_update/go_delete.sh
+++ scripts/reprocessing/move_and_update/go_delete.sh
@@ -3,4 +3,4 @@
 # Run delete with the test file
 # Deleting the old ARD's in their original location,
 # That are no longer refferenced in the ARD
-../parra_delete.sh ../scenes_old_ard_yaml.txt
+../parra_delete.sh ../old_ard_yaml.txt
