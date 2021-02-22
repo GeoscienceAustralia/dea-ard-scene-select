b1a652ffdff0c0768722e26c2f796afd84ec9fcd
Duncan Gray <duncan.gray@ga.gov.au>
2021-02-22 13:54:48 +1100

    getting ready for a production run

--- scripts/reprocessing/delete_from_yaml_list.sh
+++ scripts/reprocessing/delete_from_yaml_list.sh
@@ -6,8 +6,8 @@
 #    old_base='/g/data/xu18/ga/'
 #    new_base='/g/data/u46/users/dsg547/test_data/c3/reprocessing/ard_new'
 
-is_test=0 # True
-#is_test=1 # False
+#is_test=0 # True
+is_test=1 # False
 if [ $is_test = 0 ] ; then
     #old_base='/g/data/u46/users/dsg547/test_data/c3_reprocess/'
     old_base='/g/data/u46/users/dsg547/test_data/c3/reprocessing/ard_standard/'
@@ -19,5 +19,5 @@ ard_path="${1%/*}/"
 old_path="$old_base$ard_path"
 echo $old_path
 
-rm -r $old_path
+#rm -r $old_path
 
