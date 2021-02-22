4238825364a38b817a5776750a5da66463b14fd4
Duncan Gray <duncan.gray@ga.gov.au>
2021-02-22 14:01:55 +1100

    getting ready for a production run

--- scripts/reprocessing/move_and_update/go_delete.sh
+++ scripts/reprocessing/move_and_update/go_delete.sh
@@ -3,4 +3,4 @@
 # Run delete with the test file
 # Deleting the old ARD's in their original location,
 # That are no longer refferenced in the ARD
-../parra_delete.sh prod_2_scenes_old_ard_yaml.txt
+../parra_delete.sh ../prod_2_scenes_old_ard_yaml.txt
