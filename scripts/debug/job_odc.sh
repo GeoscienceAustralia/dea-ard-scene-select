5d1ae3afb54dbaa20d5445457caeeceac47e4955
Duncan Gray <duncan.gray@ga.gov.au>
2021-03-16 16:02:48 +1100

    minor mods

--- scripts/debug/job_odc.sh
+++ scripts/debug/job_odc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -P u46
 #PBS -q normal
-#PBS -l walltime=1:30:00,mem=1GB
+#PBS -l walltime=0:05:00,mem=1GB
 #PBS -l wd
 #PBS -l storage=gdata/v10+scratch/v10+gdata/if87+gdata/fj7+scratch/fj7+scratch/u46+gdata/u46
 #PBS -l ncpus=1
--- scripts/debug/odc_search.py
+++ scripts/debug/odc_search.py
@@ -3,7 +3,7 @@
 import datacube
 from datetime import datetime
 
-print("normal")
+#print("normal")
 print("before search =", datetime.now())
 dc = datacube.Datacube(app="gen-list")
 for dataset in dc.index.datasets.search(product="usgs_ls5t_level1_1"):
