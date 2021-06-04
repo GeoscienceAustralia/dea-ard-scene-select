773aff6a463236a5ff07b06d364ab90dab875bd4
Duncan Gray <duncan.gray@ga.gov.au>
2021-06-04 16:41:42 +1000

    getting old changes out of the way

--- scripts/debug/job_odc.sh
+++ scripts/debug/job_odc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -P u46
 #PBS -q normal
-#PBS -l walltime=0:05:00,mem=1GB
+#PBS -l walltime=0:20:00,mem=1GB
 #PBS -l wd
 #PBS -l storage=gdata/v10+scratch/v10+gdata/if87+gdata/fj7+scratch/fj7+scratch/u46+gdata/u46
 #PBS -l ncpus=1
@@ -11,4 +11,6 @@ module use /g/data/v10/private/modules/modulefiles
 
 module load dea
 
-./odc_search.py
+#strace -f -e trace=network ./odc_search.py &> strace_job.txt
+#./odc_search.py
+time find /g/data/fj7/Copernicus/Sentinel-2/MSI/L1C/ -maxdepth 3 -daystart -mtime -20 -mtime +30 -name *.zip
--- scripts/debug/odc_search.py
+++ scripts/debug/odc_search.py
@@ -6,7 +6,10 @@ from datetime import datetime
 #print("normal")
 print("before search =", datetime.now())
 dc = datacube.Datacube(app="gen-list")
-for dataset in dc.index.datasets.search(product="usgs_ls5t_level1_1"):
-    print(dataset.metadata.landsat_product_id)
+#for dataset in dc.index.datasets.search(product="usgs_ls5t_level1_1"):
+for dataset_id, landsat_product_id in dc.index.datasets.search_returning(['id', 'landsat_product_id'], product="usgs_ls5t_level1_1"):
+    print(dataset_id)
+    print(landsat_product_id)
+    #print(dataset.metadata.landsat_product_id)
     print("after search =", datetime.now())
     break
