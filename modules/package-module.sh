694284d97440211529f6bb46708988c47a8736dc
Duncan Gray <duncan.gray@ga.gov.au>
2021-11-15 16:41:52 +1100

    precommit cleaning

--- modules/package-module.sh
+++ modules/package-module.sh
@@ -23,11 +23,10 @@ export module_dir dea_module dep_module
 
 echoerr() { echo "$@" 1>&2; }
 
-if [[ $# != 1 ]] || [[ "$1" == "--help" ]];
-then
-    echoerr
-    echoerr "Usage: $0 <version>"
-    exit 1
+if [[ $# != 1 ]] || [[ "$1" == "--help" ]]; then
+	echoerr
+	echoerr "Usage: $0 <version>"
+	exit 1
 fi
 export version="$1"
 
@@ -39,35 +38,31 @@ python_version=$(python -c 'from __future__ import print_function; import sys; p
 python_major=$(python -c 'from __future__ import print_function; import sys; print(sys.version_info[0])')
 subvariant=py${python_major}
 
-
 function installrepo() {
-    destination_name=$1
-    head=${2:=develop}
-    repo=$3
+	destination_name=$1
+	head=${2:=develop}
+	repo=$3
 
-    repo_cache="cache/${destination_name}.git"
+	repo_cache="cache/${destination_name}.git"
 
-    if [ -e "${repo_cache}" ]
-    then
-        pushd "${repo_cache}"
-            git remote update
-        popd
-    else
-        git clone --mirror "${repo}" "${repo_cache}"
-    fi
+	if [ -e "${repo_cache}" ]; then
+		pushd "${repo_cache}"
+		git remote update
+		popd
+	else
+		git clone --mirror "${repo}" "${repo_cache}"
+	fi
 
-    build_dest="build/${destination_name}"
-    [ -e "${build_dest}" ] && rm -rf "${build_dest}"
-    git clone -b "${head}" "${repo_cache}" "${build_dest}"
+	build_dest="build/${destination_name}"
+	[ -e "${build_dest}" ] && rm -rf "${build_dest}"
+	git clone -b "${head}" "${repo_cache}" "${build_dest}"
 
-    pushd "${build_dest}"
-        rm -r dist build > /dev/null 2>&1 || true
-        python setup.py sdist
-        pip install dist/*.tar.gz "--prefix=${package_dest}"
-    popd
+	pushd "${build_dest}"
+	rm -r dist build >/dev/null 2>&1 || true
+	python setup.py sdist
+	pip install dist/*.tar.gz "--prefix=${package_dest}"
+	popd
 }
-
-
 
 package_name=ard-scene-select-${subvariant}-${instance}
 package_description="GA ARD scene select"
@@ -78,33 +73,29 @@ printf '# Remember to check-code.sh and run black first. #\n'
 printf '# Packaging "%s %s" to "%s" #\n' "$package_name" "$version" "$package_dest"
 
 read -p "Continue? [y/N]" -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    echo "Creating directory"
-    mkdir -v -p "${python_dest}"
-    # The destination needs to be on the path so that latter dependencies can see earlier ones
-    export PYTHONPATH=${PYTHONPATH:+${PYTHONPATH}:}${python_dest}
+echo # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	echo "Creating directory"
+	mkdir -v -p "${python_dest}"
+	# The destination needs to be on the path so that latter dependencies can see earlier ones
+	export PYTHONPATH=${PYTHONPATH:+${PYTHONPATH}:}${python_dest}
 
-    echo
-    echo "Installing ard-scene-select"
-    #installrepo ard-scene-select   archive         https://github.com/GeoscienceAustralia/dea-ard-scene-select.git
-    installrepo ard-scene-select   module-prod         https://github.com/GeoscienceAustralia/dea-ard-scene-select.git
-    #installrepo ard-scene-select   master          https://github.com/GeoscienceAustralia/dea-ard-scene-select.git
-    #installrepo wagl              develop          https://github.com/GeoscienceAustralia/wagl.git
-    echo
-    echo "Writing modulefile"
-    modulefile_dir="${module_dir}/modulefiles/${package_name}"
-    mkdir -v -p "${modulefile_dir}"
-    modulefile_dest="${modulefile_dir}/${version}"
-    envsubst < modulefile.template > "${modulefile_dest}"
-    echo "Wrote modulefile to ${modulefile_dest}"
+	echo
+	echo "Installing ard-scene-select"
+	#installrepo ard-scene-select   archive         https://github.com/GeoscienceAustralia/dea-ard-scene-select.git
+	installrepo ard-scene-select module-prod https://github.com/GeoscienceAustralia/dea-ard-scene-select.git
+	#installrepo ard-scene-select   master          https://github.com/GeoscienceAustralia/dea-ard-scene-select.git
+	#installrepo wagl              develop          https://github.com/GeoscienceAustralia/wagl.git
+	echo
+	echo "Writing modulefile"
+	modulefile_dir="${module_dir}/modulefiles/${package_name}"
+	mkdir -v -p "${modulefile_dir}"
+	modulefile_dest="${modulefile_dir}/${version}"
+	envsubst <modulefile.template >"${modulefile_dest}"
+	echo "Wrote modulefile to ${modulefile_dest}"
 fi
 
-rm -rf build > /dev/null 2>&1
-
+rm -rf build >/dev/null 2>&1
 
 echo
 echo 'Done.'
-
-    
