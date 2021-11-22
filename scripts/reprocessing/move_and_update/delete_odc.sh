7ffc2a35f27fa69c4944f09da1e5d66e07ac369e
Duncan Gray <duncan.gray@ga.gov.au>
2021-11-22 11:06:20 +1100

    update DB_hostname

--- scripts/reprocessing/move_and_update/delete_odc.sh
+++ scripts/reprocessing/move_and_update/delete_odc.sh
@@ -1,4 +1,5 @@
+#!/usr/bin/env bash
 # This doesn't work in vdi
 #module load agdc-py3/1.1.8
 
-psql -h dea-db.nci.org.au dsg547 -d dsg547_dev -a -f db_delete_odc.sql
+psql -h deadev.nci.org.au dsg547 -d dsg547_dev -a -f db_delete_odc.sql
