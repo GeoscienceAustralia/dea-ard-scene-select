2186b16348ff063b9f46b88a94faa8eb519ec999
Duncan Gray <duncan.gray@ga.gov.au>
2021-12-22 17:36:40 +1100

    update the ard-pipeline module

--- scripts/examples/examples.sh
+++ scripts/examples/examples.sh
@@ -40,9 +40,9 @@ ard_loc='/g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/prod/ard_env/p
 ard_loc='/g/data/u46/users/dsg547/sandbox/dea-ard-scene-select/scripts/prod/ard_env/prod-wagl.env'
 #time python3 ../../scene_select/ard_scene_select.py --workdir scratch/  --pkgdir  scratch/ --logdir scratch/ --project u46 --walltime 10:00:00  --env $ard_loc  --products '["usgs_ls7e_level1_1"]' --scene-limit 9999 #--find-blocked
 
-ard_loc='/g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/prod/ard_env/prod-wagl.env'
+mkdir -p scratch2
 ard_loc='/g/data/u46/users/dsg547/sandbox/dea-ard-scene-select/scripts/prod/ard_env/prod-wagl.env'
-time ard-scene-select --workdir scratch/ --pkgdir scratch/ --logdir scratch/ --project u46 --walltime 10:00:00  --env $ard_loc  --products '["usgs_ls8c_level1_1"]' --scene-limit 1 #--find-blocked
+time ard-scene-select --workdir scratch2/ --pkgdir scratch2/ --logdir scratch2/ --project u46 --walltime 10:00:00  --env $ard_loc  --products '["usgs_ls8c_level1_1"]' --scene-limit 1 --run-ard #--find-blocked
 
 #time python3 ../../scene_select/ard_scene_select.py --workdir scratch/  --pkgdir  scratch/ --logdir scratch/   --index-datacube-env $PWD/c3-samples-index-datacube.env --project u46 --walltime 05:00:00 --find-blocked #--run-ard  # --products '["usgs_ls8c_level1_1"]' --env /g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/prod/ard_env/prod-wagl.env
 
