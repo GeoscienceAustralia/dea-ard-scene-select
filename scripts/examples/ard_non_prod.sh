dcfa6a5575c432d9eee1dabb5bbb5487adce1fd7
Duncan Gray <duncan.gray@ga.gov.au>
2023-01-27 14:38:58 +1100

    add find-blocked to the ard_non_prod.sh to find potential scenes to reprocess

--- scripts/examples/ard_non_prod.sh
+++ scripts/examples/ard_non_prod.sh
@@ -16,4 +16,12 @@ module use /g/data/v10/private/modules/modulefiles
 #module load ard-scene-select-py3-dea/20200813  # Fast.
 module load ard-scene-select-py3-dea/20220516
 
-ard-scene-select --workdir scratch/  --pkgdir  scratch/ --logdir scratch/ --env /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env --project u46 --walltime 05:00:00 #--run-ard 
+ard-scene-select --find-blocked \
+--workdir scratch/  \
+--pkgdir  scratch/ \
+--logdir scratch/ \
+--env /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env \
+--project u46 \
+--find-blocked \
+# --run-ard \
+--walltime 05:00:00
