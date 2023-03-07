f2fa072a6168db17ec969de4369cdcc3b213a4af
Duncan Gray <duncan.gray@ga.gov.au>
2023-03-07 17:17:01 +1100

    add env files for ard-pipeline modules that use modis61

--- scripts/examples/ard_non_prod.sh
+++ scripts/examples/ard_non_prod.sh
@@ -14,14 +14,17 @@ module use /g/data/v10/private/modules/modulefiles
 #module load ard-scene-select-py3-dea/20200717  # fast no ancillary file checking
 #module load ard-scene-select-py3-dea/20200811  # crap and slow
 #module load ard-scene-select-py3-dea/20200813  # Fast.
-module load ard-scene-select-py3-dea/20220516
+module load ard-scene-select-py3-dea/20220922
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 ard-scene-select \
 --workdir scratch/  \
 --pkgdir  scratch/ \
 --logdir scratch/ \
---env /g/data/v10/Landsat-Collection-3-ops/OFFICIAL/Collection-3_5.4.1.env \
+--env $DIR/brdf61_prod-wagl.env \
 --project u46 \
---find-blocked \
+--brdfdir  /g/data/v10/eoancillarydata-2/BRDF/MCD43A1.061 \
+#--find-blocked \
 # --run-ard \
 --walltime 05:00:00
