8e6ae4e9c186572de5c0bc88d02976d7f68865b8
Gordon Yeong <123341213+gordonyeong@users.noreply.github.com>
2023-03-23 12:39:08 +1100

    DSNS-79/102: successfully tested dea package 20221025 and the h5 compression filter, 20230215 - STABLE (#28)
    h5 compression filter, 20230215. Can confirm that
       they are stable.
     1. changes put in to create a dev package to test the new
        dea module 2022 10 25 and h5  compression module
     2. on dea-ard-scene-select/modules/, created the dev
        package by updating package_module.sh and ran go.sh
     3. created dea-ard-scene-select/scripts/tickets/DSNS-102/load_new_test_module_20221025.sh
        which loads the correct versions of the dea package
        and h5-compression-filter packages. This file is from example.sh
     4. the scripts for creating packages, go.sh and package-module.sh
        have been updated to cater for production and non production runs. By default,
        the runs are non production. If a production run is required,
        the --prod flag is to be provided to go.sh

--- modules/go.sh
+++ modules/go.sh
@@ -2,5 +2,13 @@
 # do a fresh login before running this to avoid issues from
 # modules already loaded
 
-./package-module.sh "${2:-$(date +'%Y%m%d')}"
-#./package-module.sh  dev_${2:-$(date +'%Y%m%d')}
+# This script, by default, would run in the non production
+# environment. If a production run is required,
+# a --prod flag is to be provided to this script.
+if [[ $1 == "--prod" ]]; then
+    # production version
+    ./package-module.sh "${2:-$(date +'%Y%m%d')}" --prod
+else
+    # dev version
+    ./package-module.sh  dev_${2:-$(date +'%Y%m%d')}
+fi
