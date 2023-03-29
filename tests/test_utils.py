9a3039241f20c965af24a582dba48b8f0317230a
Duncan Gray <duncan.gray@ga.gov.au>
2023-03-29 12:34:23 +1100

    PR recommendation

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 from pathlib import Path
 from unittest.mock import Mock
 from scene_select.utils import calc_file_path
@@ -21,8 +22,8 @@ def test_local_path():
 
     ls_l1_dataset = Mock()
     the_path = "/this/path/"
-    ls_l1_dataset.local_path = Path(the_path + "LC08_T2.odc-metadata.yaml")
+    ls_l1_dataset.local_path = Path(os.path.join(the_path, "LC08_T2.odc-metadata.yaml"))
     product_id = "LC08_T2"
-    actual = the_path + product_id + ".tar"
+    actual = os.path.join(the_path, product_id + ".tar")
     result = calc_file_path(ls_l1_dataset, product_id)
     assert result == actual
