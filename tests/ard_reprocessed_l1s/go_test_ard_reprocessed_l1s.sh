1337247bd78a6867bebac3dbfb1e2170fc8d1eaf
Duncan Gray <duncan.gray@ga.gov.au>
2023-05-11 13:33:47 +1000

    tab to space fix

--- tests/ard_reprocessed_l1s/go_test_ard_reprocessed_l1s.sh
+++ tests/ard_reprocessed_l1s/go_test_ard_reprocessed_l1s.sh
@@ -4,7 +4,7 @@ if [[ $HOSTNAME == *"gadi"* ]]; then
   echo "gadi - NCI"
   module use /g/data/v10/public/modules/modulefiles
   module use /g/data/v10/private/modules/modulefiles
-	module use /g/data/u46/users/$USER/devmodules/modulefiles
+  module use /g/data/u46/users/$USER/devmodules/modulefiles
 
   #module load dea/20221025
   module load ard-scene-select-py3-dea/dev_20230511
