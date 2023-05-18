b7264becb666f07fc89dc39c6ef622196a7b4347
Duncan Gray <duncan.gray@ga.gov.au>
2023-05-18 15:51:09 +1000

    DSNS-157: Test suite to test new dea package being used in datacube.

---
+++ tests/scripts/non-prod/db_delete_odc.sql
@@ -0,0 +1,8 @@
+SELECT id FROM agdc.dataset;
+BEGIN;
+DELETE FROM agdc.dataset_location;
+DELETE FROM agdc.dataset_source;
+DELETE FROM agdc.dataset;
+DELETE FROM agdc.dataset_type;
+DELETE FROM agdc.metadata_type;
+COMMIT;
---
+++ tests/scripts/non-prod/overall.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+./delete_odc.sh
+./db_index.sh
+./check_db.sh
+./nci_examples.sh
