923071be43f175f7e5e4487ee9d1dd0871c5b00c
Gordon Yeong <gordon.yeong@ga.gov.au>
2023-05-23 16:15:00 +1000

    DSNS-157: Test suite to test new dea package being used in datacube.

---
+++ tests/ard_tests/check_db.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [[ $HOSTNAME == *"gadi"* ]]; then
+  echo "gadi - NCI"
+  module use /g/data/v10/public/modules/modulefiles
+  module use /g/data/v10/private/modules/modulefiles
+
+  module load dea
+  #module load ard-pipeline/devv2.1
+  ODCCONF="--config ${USER}_dev.conf"
+  
+else
+  echo "not NCI"
+  ODCCONF="--config ${USER}_local.conf"
+  # datacube -v  $ODCCONF system init
+fi
+
+echo product='usgs_ls9c_level1_2'
+datacube  $ODCCONF dataset search product='usgs_ls9c_level1_2' | grep '^id: '
+
+echo product='ga_ls9c_ard_3'
+datacube  $ODCCONF dataset search product='ga_ls9c_ard_3' | grep '^id: '
