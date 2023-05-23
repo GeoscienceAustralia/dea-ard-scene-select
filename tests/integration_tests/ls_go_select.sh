beb611a0189de97553d6f8568ba8efcbbf78f84f
Duncan Gray <duncan.gray@ga.gov.au>
2023-05-23 12:52:11 +1000

    test module fix (#39)

--- tests/integration_tests/ls_go_select.sh
+++ tests/integration_tests/ls_go_select.sh
@@ -7,9 +7,9 @@ if [[ $HOSTNAME == *"gadi"* ]]; then
     module use /g/data/u46/users/$USER/devmodules/modulefiles
 
     # Needed for pytest to be loaded
-    module load dea/20221025
+    # module load dea/20221025
     # module load ard-scene-select-py3-dea/dev_20230522
-    # module load ard-scene-select-py3-dea/20230515
+    module load ard-scene-select-py3-dea/20230330
 
     SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
     SSPATH="$SCRIPT_DIR/../.."
