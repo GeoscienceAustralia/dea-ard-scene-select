a3c2216d8358dbbb813e52994a3e73ee937d383d
Duncan Gray <duncan.gray@ga.gov.au>
2023-06-13 14:51:06 +1000

    support running tests not at NCI more

--- tests/do_tests.sh
+++ tests/do_tests.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-module use /g/data/v10/public/modules/modulefiles
-module use /g/data/v10/private/modules/modulefiles
+if [[ $HOSTNAME == *"gadi"* ]]; then
+    module use /g/data/v10/public/modules/modulefiles
+    module use /g/data/v10/private/modules/modulefiles
 
-# Does not seem to be needed, but this has be flakey in the past
-# so I'm leaving it in for now as a comment
-# module load h5-compression-filters/20200612
+    # Does not seem to be needed, but this has be flakey in the past
+    # so I'm leaving it in for now as a comment
+    # module load h5-compression-filters/20200612
 
-module load ard-pipeline/20230306-l9
+    module load ard-pipeline/20230306-l9
+fi
+
+
+if [[ $HOSTNAME == *"LAPTOP-UOJEO8EI"* ]]; then
+    echo "duncans laptop"
+    echo "conda activate /home/duncan/bin/miniconda3/envs/odc2020"
+    echo "test_check_ancillary.py fails with No module named 'wagl'"
+fi
 
 SSPATH=$PWD/../
 
