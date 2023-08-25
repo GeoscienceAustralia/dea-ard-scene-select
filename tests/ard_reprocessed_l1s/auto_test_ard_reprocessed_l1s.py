1f0aa2e3f9f5607b82d029472e226df93afe9024
Duncan Gray <duncan.gray@ga.gov.au>
2023-08-25 11:13:34 +1000

    simplify more

--- tests/ard_reprocessed_l1s/auto_test_ard_reprocessed_l1s.py
+++ tests/ard_reprocessed_l1s/auto_test_ard_reprocessed_l1s.py
@@ -40,7 +40,6 @@ from typing import Dict
 import datacube
 from scene_select.ard_reprocessed_l1s import (
     ard_reprocessed_l1s,
-    DIR_TEMPLATE,
     move_blocked,
 )
 from scene_select.do_ard import ARCHIVE_FILE, ODC_FILTERED_FILE, PBS_ARD_FILE
@@ -49,7 +48,7 @@ TEST_DATA_DIR = Path(__file__).parent.joinpath("..", "test_data").resolve()
 ODC_YAML_DIR = TEST_DATA_DIR.joinpath("odc_setup").resolve()
 SCENES_DIR = TEST_DATA_DIR.joinpath( "ls9_reprocessing").resolve()
 
-NEW_BASE_PATH = SCENES_DIR.joinpath("moved")
+MOVED_PATH = SCENES_DIR.joinpath("moved")
 
 METADATA_TYPES = [
     ODC_YAML_DIR / "eo3_landsat_l1.odc-type.yaml",
@@ -81,18 +80,18 @@ DATASETS = [
     / "ga_ls9c_ard_3/095/074/2022/06/26/ga_ls9c_ard_3-2-1_095074_2022-06-26_final.odc-metadata.yaml",
 ]
 
+pytestmark = pytest.mark.usefixtures("auto_odc_db")
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_local_directories_and_files():
 
     # Delete and recreate the file structure
-    test_data_moved = os.path.join(SCENES_DIR, "moved", "ga_ls9c_ard_3")
     test_data_ga = os.path.join(SCENES_DIR, "ga_ls9c_ard_3")
     test_data_raw = os.path.join(SCENES_DIR, "a_ga_ls9c_ard_3_raw")
-    shutil.rmtree(test_data_moved, ignore_errors=True)
+    shutil.rmtree(MOVED_PATH, ignore_errors=True)
     shutil.rmtree(test_data_ga, ignore_errors=True)
     shutil.copytree(test_data_raw, test_data_ga)
-    os.makedirs(os.path.join(SCENES_DIR, "moved"), exist_ok=True)
+    os.makedirs(MOVED_PATH, exist_ok=True)
 
 
 @pytest.fixture
@@ -104,147 +103,126 @@ def archive(odc_test_db):
         odc_test_db.index.datasets.archive([entry])
 
 
-pytestmark = pytest.mark.usefixtures("auto_odc_db")
-
-
-def test_ard_reprocessed_l1s(archive, odc_test_db: datacube.Datacube):
+def test_ard_reprocessed_l1s(archive, odc_test_db: datacube.Datacube, tmpdir):
     """Test the ard_reprocessed_l1s function."""
 
     # Set this to true and all the results
     # of the scene select run will be displayed
     SCENE_SELECT_PROCESS_RUN_VERBOSE = False
 
-    new_base_path = SCENES_DIR.joinpath("moved")
-    current_base_path = SCENES_DIR
-    SCRATCH_DIR = Path(__file__).parent.joinpath("scratch")
+    SCRATCH_DIR = Path(tmpdir)
 
-    # in bash
-    # hex=$(openssl rand -hex 3)
-    jobdir = SCRATCH_DIR.joinpath(DIR_TEMPLATE.format(jobid=uuid.uuid4().hex[0:6]))
+    jobdir = SCRATCH_DIR.joinpath('jobdir')
     jobdir.mkdir(exist_ok=True)
-
-    product = "ga_ls9c_ard_3"
-
-    scene_limit = 2
-    run_ard = False
 
     cmd_params = [
         "--current-base-path",
-        str(current_base_path.resolve()),
+        str(SCENES_DIR),
         "--new-base-path",
-        str(new_base_path.resolve()),
+        str(MOVED_PATH),
         # We need to provide our explicit jobdir so that
         # we can get hold of the ard to process file
         "--jobdir",
         str(jobdir),
-        "--product",
-        product,
-        "--scene-limit",
-        scene_limit,
         "--logdir",
         SCRATCH_DIR,
     ]
 
-    try:
-        runner = CliRunner()
-        result = runner.invoke(ard_reprocessed_l1s, cmd_params)
-        if SCENE_SELECT_PROCESS_RUN_VERBOSE:
-            print("***** results output ******")
-            print(result.output)
-            print("***** results exception ******")
-            print(result.exception)
-            print("***** results end ******")
+    runner = CliRunner()
+    result = runner.invoke(ard_reprocessed_l1s, cmd_params)
+    if result.exception is not None:
+        pytest.fail(f"Unexpected exception: {result.exception} \n {result.output}")
 
-        # Assert a few things
-        # Two dirs have been moved.  These are the previous datasets
-        # that we sent in for reprocessing.
+    if SCENE_SELECT_PROCESS_RUN_VERBOSE:
+        print("***** results output ******")
+        print(result.output)
+        print("***** results exception ******")
+        print(result.exception)
+        print("***** results end ******")
 
-        new_dir_06_21 = SCENES_DIR.joinpath(
-            "moved", "ga_ls9c_ard_3", "092", "081", "2022", "06", "21"
-        )
-        fname_06_21 = new_dir_06_21.joinpath(
-            "ga_ls9c_ard_3-2-1_092081_2022-06-21_final.odc-metadata.yaml"
-        )
+    # Assert a few things
+    # Two dirs have been moved.  These are the previous datasets
+    # that we sent in for reprocessing.
 
-        assert os.path.isfile(
-            fname_06_21
-        ), f"The yaml file, '{fname_06_21}' has been moved, for a different scene"
+    new_dir_06_21 = MOVED_PATH.joinpath(
+        "ga_ls9c_ard_3", "092", "081", "2022", "06", "21"
+    )
+    fname_06_21 = new_dir_06_21.joinpath(
+        "ga_ls9c_ard_3-2-1_092081_2022-06-21_final.odc-metadata.yaml"
+    )
 
-        new_dir_06_27 = SCENES_DIR.joinpath(
-            "moved", "ga_ls9c_ard_3", "102", "076", "2022", "06", "27"
-        )
-        yaml_fname_06_27 = new_dir_06_27.joinpath(
-            "ga_ls9c_ard_3-2-1_102076_2022-06-27_final.odc-metadata.yaml"
-        )
+    assert os.path.isfile(
+        fname_06_21
+    ), f"The yaml file, '{fname_06_21}' has been moved, for a different scene"
 
-        assert os.path.isfile(
-            yaml_fname_06_27
-        ), f"The yaml file, '{yaml_fname_06_27}' has been moved"
+    new_dir_06_27 = MOVED_PATH.joinpath(
+        "ga_ls9c_ard_3", "102", "076", "2022", "06", "27"
+    )
+    yaml_fname_06_27 = new_dir_06_27.joinpath(
+        "ga_ls9c_ard_3-2-1_102076_2022-06-27_final.odc-metadata.yaml"
+    )
 
-        ard_id_06_27 = "d9a499d1-1abd-4ed1-8411-d584ca45de25"
-        ard_dataset = odc_test_db.index.datasets.get(ard_id_06_27)
-        local_path = Path(ard_dataset.local_path).resolve()
-        assert str(local_path) == str(
-            yaml_fname_06_27
-        ), "The OCD ARD path has not been updated"
+    assert os.path.isfile(
+        yaml_fname_06_27
+    ), f"The yaml file, '{yaml_fname_06_27}' has been moved"
 
-        # uuids have been written to an archive file
-        filename = jobdir.joinpath(ARCHIVE_FILE)
+    ard_id_06_27 = "d9a499d1-1abd-4ed1-8411-d584ca45de25"
+    ard_dataset = odc_test_db.index.datasets.get(ard_id_06_27)
+    local_path = Path(ard_dataset.local_path).resolve()
+    assert str(local_path) == str(
+        yaml_fname_06_27
+    ), "The OCD ARD path has not been updated"
 
-        assert os.path.isfile(
-            filename
-        ), f"There is no UUIDs archive list file, {filename}"
-        with open(filename, "r", encoding="utf-8") as f:
-            temp = f.read().splitlines()
+    # uuids have been written to an archive file
+    filename = jobdir.joinpath(ARCHIVE_FILE)
 
-        assert sorted(
-            [
-                "3de6cb49-60da-4160-802b-65903dcbbac8",
-                "d9a499d1-1abd-4ed1-8411-d584ca45de25",
-            ]
-        ) == sorted(temp), "The correct uuids have been written to the archive file"
+    assert os.path.isfile(
+        filename
+    ), f"There is no UUIDs archive list file, {filename}"
+    with open(filename, "r", encoding="utf-8") as f:
+        temp = f.read().splitlines()
 
-        odc_filename = jobdir.joinpath(ODC_FILTERED_FILE)
+    assert sorted(
+        [
+            "3de6cb49-60da-4160-802b-65903dcbbac8",
+            "d9a499d1-1abd-4ed1-8411-d584ca45de25",
+        ]
+    ) == sorted(temp), "The correct uuids have been written to the archive file"
 
-        with open(odc_filename, "r", encoding="utf-8") as f:
-            temp = f.read().splitlines()
+    odc_filename = jobdir.joinpath(ODC_FILTERED_FILE)
 
-        if "HOSTNAME" in os.environ and "gadi" in os.environ["HOSTNAME"]:
-            base_location = Path("/g/data/da82/AODH/USGS/L1/Landsat/C2/")
-        else:
-            base_location = SCENES_DIR.joinpath("l1_Landsat_C2")
+    with open(odc_filename, "r", encoding="utf-8") as f:
+        temp = f.read().splitlines()
 
-        a_l1_tar = base_location.joinpath(
-            "092_081",
-            "LC90920812022172",
-            "LC09_L1TP_092081_20220621_20220802_02_T1.tar",
-        )
-        b_l1_tar = base_location.joinpath(
-            "102_076",
-            "LC91020762022178",
-            "LC09_L1TP_102076_20220627_20220802_02_T1.tar",
-        )
+    if "HOSTNAME" in os.environ and "gadi" in os.environ["HOSTNAME"]:
+        base_location = Path("/g/data/da82/AODH/USGS/L1/Landsat/C2/")
+    else:
+        base_location = SCENES_DIR.joinpath("l1_Landsat_C2")
 
-        assert os.path.isfile(
-            filename
-        ), f"ard to be processed reference file {filename} does not exist"
-        assert os.path.isfile(a_l1_tar), f"{a_l1_tar} file does not exist"
-        assert os.path.isfile(b_l1_tar), f"{b_l1_tar} file does not exist"
-        left = sorted([str(a_l1_tar), str(b_l1_tar)])
-        right = sorted(temp)
-        assert sorted([str(a_l1_tar), str(b_l1_tar)]) == sorted(temp), (
-            "The correct l1 tars have been written to the scene select"
-            " file, scenes_to_ARD_process.txt"
-        )
-        filename = jobdir.joinpath(PBS_ARD_FILE)
-        assert os.path.isfile(filename), "There is a run ard pbs file"
-    except Exception as exception_message:
-        # this traps errors thrown out by the scene_select() function
-        # such as bad parameter types or NoneType when the argument
-        # is expected to be non-None
-        pytest.fail(f"Unexpected exception: {exception_message}")
-    finally:
-        shutil.rmtree(jobdir)
+    a_l1_tar = base_location.joinpath(
+        "092_081",
+        "LC90920812022172",
+        "LC09_L1TP_092081_20220621_20220802_02_T1.tar",
+    )
+    b_l1_tar = base_location.joinpath(
+        "102_076",
+        "LC91020762022178",
+        "LC09_L1TP_102076_20220627_20220802_02_T1.tar",
+    )
+
+    assert os.path.isfile(
+        filename
+    ), f"ard to be processed reference file {filename} does not exist"
+    assert os.path.isfile(a_l1_tar), f"{a_l1_tar} file does not exist"
+    assert os.path.isfile(b_l1_tar), f"{b_l1_tar} file does not exist"
+    left = sorted([str(a_l1_tar), str(b_l1_tar)])
+    right = sorted(temp)
+    assert sorted([str(a_l1_tar), str(b_l1_tar)]) == sorted(temp), (
+        "The correct l1 tars have been written to the scene select"
+        " file, scenes_to_ARD_process.txt"
+    )
+    filename = jobdir.joinpath(PBS_ARD_FILE)
+    assert os.path.isfile(filename), "There is a run ard pbs file"
 
 
 def get_blocked_scene_ard() -> str:
@@ -263,10 +241,7 @@ def get_file_paths_for_test_move_blocked() -> Dict:
             "27",
             "ga_ls9c_ard_3-2-1_102076_2022-06-27_final.odc-metadata.yaml",
         ),
-        "current_base_path": SCENES_DIR,
-        "new_base_path": SCENES_DIR.joinpath("moved"),
-        "yaml_fname_06_27": SCENES_DIR.joinpath(
-            "moved",
+        "yaml_fname_06_27": MOVED_PATH.joinpath(
             "ga_ls9c_ard_3",
             "102",
             "076",
@@ -298,8 +273,8 @@ def test_move_blocked(archive, odc_test_db: datacube.Datacube):
 
     l1_zips, uuids2archive = move_blocked(
         blocked_scenes,
-        file_paths["current_base_path"],
-        file_paths["new_base_path"],
+        SCENES_DIR,
+        MOVED_PATH,
     )
 
     # Test that in the case that
@@ -338,8 +313,8 @@ def test_move_blocked(archive, odc_test_db: datacube.Datacube):
 
     l1_zips, uuids2archive = move_blocked(
         blocked_scenes,
-        file_paths["current_base_path"].resolve(),
-        file_paths["new_base_path"].resolve(),
+        SCENES_DIR,
+        MOVED_PATH,
     )
 
     # Assert the dir ... is still there
