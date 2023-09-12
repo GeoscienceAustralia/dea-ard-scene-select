6200fc739716770b4b3e1315f00459b6d4a86dce
Duncan Gray <duncan.gray@ga.gov.au>
2023-09-12 17:03:23 +1000

    get tests running locally

--- tests/integration_tests/launch_test.sh
+++ tests/integration_tests/launch_test.sh
@@ -15,14 +15,21 @@ if [[ $HOSTNAME == *"gadi"* ]]; then
   # module load ard-scene-select-py3-dea/dev_20230522
   module load ard-scene-select-py3-dea/20230616  # This is from ls_go_select.sh
 
-else
-  echo "This test needs to run on gadi (nci)"
-  exit 1
+
+  export ODC_TEST_DB_URL=postgresql://$USER"@deadev.nci.org.au/"$USER"_automated_testing"
+fi
+
+if [[ $HOSTNAME == *"LAPTOP-UOJEO8EI"* ]]; then
+  echo "duncans laptop"
+  echo "conda activate /home/duncan/bin/miniconda3/envs/odc2020"
+  echo "note the conda env is broken"
+  echo "sudo service postgresql start"
+
+  export ODC_TEST_DB_URL=postgresql://$USER"@local/"$USER"_local"
 fi
 
 cd "$(dirname "$0")"
 
-export ODC_TEST_DB_URL=postgresql://$USER"@deadev.nci.org.au/"$USER"_automated_testing"
 
 if [ -e $1 ]; then
   echo " Going to run $1"
