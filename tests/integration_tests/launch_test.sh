0ce8bfe10d37b61916475bb4d3f350c4917185d4
Gordon Yeong <gordon.yeong@ga.gov.au>
2023-09-20 11:52:41 +1000

    DSNS-230:     1. Introduced new integration tests for s2 data - R3.2 Process a scene if the child is interim and ancill data is there     2. Updated util.py's get_extracted_path() to work with s2. Also tested the existing tests to ensure they still work.

--- tests/integration_tests/launch_test.sh
+++ tests/integration_tests/launch_test.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-# This is a test launcher whereby we can give it any test 
+# This is a test launcher whereby we can give it any test
 # or *.py to run on pytest
 
 host=localhost
+port=5432
 if [[ $HOSTNAME == *"gadi"* ]]; then
   echo "gadi - NCI"
   module use /g/data/v10/public/modules/modulefiles
@@ -19,15 +20,37 @@ if [[ $HOSTNAME == *"gadi"* ]]; then
 
 fi
 
-export ODC_TEST_DB_URL=postgresql://$USER"@"$host"/"$USER"_automated_testing"
+#export the following so that the s2 tests can directly manipulate the
+# local datacube with regards to dataset
+export ODC_TEST_DB_URL=postgresql://$USER"@"$host":"$port"/"$USER"_automated_testing"
+export ODC_HOST=$host
+
+# Create a temporary configuration file
+temp_config_file=$(mktemp)
+
+# Define the contents of the configuration file
+config_file_contents="[datacube]
+db_hostname: deadev.nci.org.au
+db_port: 5432
+db_database: ${USER}_automated_testing
+"
+
+# Write the contents to the temporary configuration file
+echo -e "$config_file_contents" > "$temp_config_file"
+
+# Export the path of the temporary configuration file as an environment variable
+export AUTOMATED_TEST_CONFIG_FILE="$temp_config_file"
+
+# Print the path of the temporary configuration file
+echo "The temp config file path is $temp_config_file"
 
 if [[ $HOSTNAME == *"LAPTOP-UOJEO8EI"* ]]; then
   echo "duncans laptop"
   echo "conda activate /home/duncan/bin/miniconda3/envs/odc2020"
   echo "note the conda env is broken"
   echo "sudo service postgresql start"
-
 fi
+
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SSPATH=$DIR/../../
@@ -36,8 +59,8 @@ SSPATH=$DIR/../../
 #echo $PYTHONPATH
 export PYTHONPATH=$PYTHONPATH
 
-cd "$(dirname "$0")"
 
+cd "$(dirname "$0")"
 
 if [ -e $1 ]; then
   echo " Going to run $1"
