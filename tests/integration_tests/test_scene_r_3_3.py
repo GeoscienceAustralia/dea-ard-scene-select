4a80e6bdeadf3beeb6fd66e11ea191a9651879b8
Gordon Yeong <gordon.yeong@ga.gov.au>
2023-10-02 12:20:34 +1100

    DSNS-231: Introduced integration tests to reflect R3.3 as detailed below.   - R3.3 Filter out the l1 scene if there is already an ARD scene with the same scene_id, that is un-archived.   - test_scene_r_2_7.py - removed as it seems to be a copy of test_scene_r_2_6.py

--- tests/integration_tests/test_scene_r_3_3.py
+++ tests/integration_tests/test_scene_r_3_3.py
@@ -1,6 +1,7 @@
 """
-    DSNS-236
-    R2.6 Filter out l1 scenes if the dataset has a child and the child is not archived
+    DSNS-231
+       R3.3 Filter out the l1 scene if there is already an ARD scene with the
+       same scene_id, that is un-archived.
 """
 from pathlib import Path
 import os
@@ -9,7 +10,7 @@ from click.testing import CliRunner
 import pytest
 from scene_select.ard_scene_select import scene_select, GEN_LOG_FILE
 from scene_select.do_ard import ODC_FILTERED_FILE
-
+import datacube
 from util import (
     get_list_from_file,
 )
@@ -28,7 +29,6 @@ PRODUCTS_DIR = (
 
 PRODUCTS = [
     os.path.join(PRODUCTS_DIR, "l1_ls8.odc-product.yaml"),
-    os.path.join(PRODUCTS_DIR, "l1_ls8_c2.odc-product.yaml"),
     os.path.join(PRODUCTS_DIR, "ard_ls8.odc-product.yaml"),
 ]
 
@@ -44,33 +44,28 @@ DATASETS_DIR = (
 DATASETS = [
     os.path.join(
         DATASETS_DIR,
-        "c3/LC80900742020304/LC08_L1GT_090074_20201030_20201106_01_T2.odc-metadata.yaml",
+        "c3/LC81150802019349/LC08_L1TP_115080_20191215_20191226_01_T1.odc-metadata.yaml",
     ),
     os.path.join(
         DATASETS_DIR,
-        "c3/ARD_LC80900742020304_final/ga_ls8c_ard_3-1-0_090074_2020-10-30_final.odc-metadata.yaml",
+        "c3/LC81150802019349/LC08_L1TP_115080_20191215_20201023_01_T1.odc-metadata.yaml",
     ),
     os.path.join(
         DATASETS_DIR,
-        "c3/LC80960702022336_do_interim/LC08_L1TP_096070_20221202_20221212_02_T1.odc-metadata.yaml",
-    ),
-    os.path.join(
-        DATASETS_DIR,
-        "c3/LC80960702022336_ard/ga_ls8c_ard_3-2-1_096070_2022-12-02_interim.odc-metadata.yaml",
+        "c3/ARD_LC81150802019349_old/ga_ls8c_ard_3-1-0_115080_2019-12-15_final.odc-metadata.yaml",
     ),
 ]
 
 pytestmark = pytest.mark.usefixtures("auto_odc_db")
 
 
-def test_scene_filtering_r_2_6(tmp_path):
+def test_scene_r_3_3(odc_test_db: datacube.Datacube, tmp_path):
     """
     This is the collective test that implements the requirement as
     defined at the top of this test suite.
     """
+    odc_test_db.index.datasets.archive(["760315b3-e147-5db2-bb7f-0e52efd4453d"])
 
-    # Observe that there is a date exclusion filter passed
-    # into the process
     cmd_params = [
         "--config",
         os.environ.get("AUTOMATED_TEST_CONFIG_FILE"),
@@ -88,6 +83,26 @@ def test_scene_filtering_r_2_6(tmp_path):
 
     assert result.exit_code == 0, "The scene_select process failed to execute"
 
+    # Use glob to search for the scenes_to_ARD_process.txt file
+    # within filter-jobid-* directories
+    matching_files = list(Path(tmp_path).glob("filter-jobid-*/" + ODC_FILTERED_FILE))
+
+    # There's only ever 1 copy of scenes_to_ARD_process.txt after
+    # successfully processing
+    assert matching_files and matching_files[0] is not None, (
+        "Scene select failed. List of entries to process is not available - ",
+        f"{matching_files}",
+    )
+    ards_to_process = get_list_from_file(matching_files[0])
+
+    # Given that the run should have no ards to process, we expect
+    # an empty scenes_to_ARD_process.txt file.
+
+    assert len(ards_to_process) == 0, (
+        "Ard entries to process exist when we are not expecting anything",
+        " to be there",
+    )
+
     # Use glob to search for the log file
     # within filter-jobid-* directories
     matching_files = list(Path(tmp_path).glob("filter-jobid-*/" + GEN_LOG_FILE))
@@ -97,46 +112,25 @@ def test_scene_filtering_r_2_6(tmp_path):
         matching_files and matching_files[0] is not None
     ), f"Scene select failed. Log is not available - {matching_files}"
 
-    assert matching_files and matching_files[0] is not None, (
-        "Scene select failed. List of entries to process is not available -",
-        f" {matching_files}",
-    )
-
     found_log_line = False
+
     with open(matching_files[0], encoding="utf-8") as ard_log_file:
         for line in ard_log_file:
             try:
                 jline = json.loads(line)
                 if (
-                    all(key in jline for key in ("reason", "dataset_id", "event"))
+                    all(key in jline for key in ("reason", "event", "landsat_scene_id"))
                     and jline["reason"] == "The scene has been processed"
-                    and jline["dataset_id"] == "91f2fbd8-8ad5-550b-a62c-d819e1a4baaa"
                     and jline["event"] == "scene removed"
+                    and jline["landsat_scene_id"]
+                    == "LC08_L1TP_115080_20191215_20201023_01_T1"
                 ):
                     found_log_line = True
                     break
             except json.JSONDecodeError as error_string:
                 print(f"Error decoding JSON: {error_string}")
-    assert (
-        found_log_line
-    ), "Landsat scene still selected despite its date is being excluded"
 
-    # Use glob to search for the scenes_to_ARD_process.txt file
-    # within filter-jobid-* directories
-    matching_files = list(Path(tmp_path).glob("filter-jobid-*/" + ODC_FILTERED_FILE))
-
-    # There's only ever 1 copy of scenes_to_ARD_process.txt after
-    # successfully processing
-    assert matching_files and matching_files[0] is not None, (
-        f"Scene select failed. List of entries to process is not available :{ODC_FILTERED_FILE} "
-        f"- {matching_files}"
-    )
-    ards_to_process = get_list_from_file(matching_files[0])
-
-    # Given that the run should have no ards to process, we expect
-    # an empty scenes_to_ARD_process.txt file.
-
-    assert len(ards_to_process) == 0, (
-        "Ard entries to process exist when we are not expecting "
-        f"anything to be there, {ards_to_process}"
+    assert found_log_line, (
+        "landsat scene still selected despite there is already an ARD scene",
+        " with the same id and its initial scene had been archived",
     )
