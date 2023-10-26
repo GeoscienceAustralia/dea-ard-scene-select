67f5b36fd213010572d28196d8a82d42a9eeb5d4
Duncan Gray <duncan.gray@ga.gov.au>
2023-10-26 17:11:40 +1100

    minor fix

--- tests/dynamic_config_file.sh
+++ tests/dynamic_config_file.sh
@@ -3,7 +3,6 @@
 # If a host name is given (at the moment, it's typically 'gadi'),
 # it reflects an NCI run.
 generate_dynamic_config_file() {
-    local hostname="local"
     local db_hostname="localhost"
     local config_file="${USER}_dev.conf"
 
