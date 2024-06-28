8eaf1ad93d56fe07855e4cf864f27666a2895783
Jeremy Hooke <jez@stulk.com>
2024-06-28 13:44:48 +1000

    format

--- scene_select/scene_filters.py
+++ scene_select/scene_filters.py
@@ -16,6 +16,7 @@ Four types of expressions are available:
 Where DATE or DATE-RANGE is one of YYYY, YYYY-MM or YYYY-MM-DD
 and START, END are either numbers or dates.
 """
+
 import datetime
 import warnings
 from typing import Any, NamedTuple
@@ -88,10 +89,14 @@ search_grammar = r"""
 def identity(x):
     return x
 
+
 class GreaterThan(NamedTuple):
     value: Any
+
+
 class LessThan(NamedTuple):
     value: Any
+
 
 @v_args(inline=True)
 class TreeToSearchExprs(Transformer):
@@ -109,23 +114,26 @@ class TreeToSearchExprs(Transformer):
         return {str(time_field): date_gt}
 
     def time_lt_expr(self, time_field, date_lt):
-
         return {str(time_field): date_lt}
 
     def field_gt_expr(self, field, value):
-        if not field.endswith('_version'):
+        if not field.endswith("_version"):
             # Underlying ODC doesn't support this operator for fields.
             # You have to use "in" expressions for numbers.
-            raise ValueError(f"Can only use > with software version "
-                             f"fields (or dates) for now, sorry. Tried {field}")
+            raise ValueError(
+                f"Can only use > with software version "
+                f"fields (or dates) for now, sorry. Tried {field}"
+            )
         return {str(field): GreaterThan(value)}
 
     def field_lt_expr(self, field, value):
-        if not field.endswith('_version'):
+        if not field.endswith("_version"):
             # Underlying ODC doesn't support this operator for fields.
             # You have to use "in" expressions for numbers.
-            raise ValueError(f"Can only use < with software version "
-                             f"fields (or dates) for now, sorry. Tried {field}")
+            raise ValueError(
+                f"Can only use < with software version "
+                f"fields (or dates) for now, sorry. Tried {field}"
+            )
         return {str(field): LessThan(value)}
 
     # Convert the literals
@@ -162,23 +170,26 @@ class TreeToSearchExprs(Transformer):
 
 def parse_expressions(*expression_text):
     expr_parser = Lark(search_grammar)
-    tree = expr_parser.parse(' '.join(expression_text))
+    tree = expr_parser.parse(" ".join(expression_text))
     return TreeToSearchExprs().transform(tree)
 
 
 def test_parser():
-    assert parse_expressions('platform = "LANDSAT_8"') == {'platform': 'LANDSAT_8'}
+    assert parse_expressions('platform = "LANDSAT_8"') == {"platform": "LANDSAT_8"}
 
     # Wagl version tests
-    assert parse_expressions('wagl_version in ["1.2.3", "3.4.5"]') == {'wagl_version': Range('1.2.3', '3.4.5')}
-    assert parse_expressions('wagl_version < "1.2.3.dev4"') == {'wagl_version': Range(None, '1.2.3.dev4')}
+    assert parse_expressions('wagl_version in ["1.2.3", "3.4.5"]') == {
+        "wagl_version": Range("1.2.3", "3.4.5")
+    }
+    assert parse_expressions('wagl_version < "1.2.3.dev4"') == {
+        "wagl_version": Range(None, "1.2.3.dev4")
+    }
 
 
 def _time_to_search_dims(time_range):
-
     tr_start, tr_end = time_range, time_range
 
-    if hasattr(time_range, '__iter__') and not isinstance(time_range, str):
+    if hasattr(time_range, "__iter__") and not isinstance(time_range, str):
         tmp = list(time_range)
         if len(tmp) > 2:
             raise ValueError("Please supply start and end date only for time query")
@@ -192,7 +203,7 @@ def _time_to_search_dims(time_range):
         start = datetime.datetime.fromtimestamp(0)
     elif not isinstance(tr_start, datetime.datetime):
         # convert to datetime.datetime
-        if hasattr(tr_start, 'isoformat'):
+        if hasattr(tr_start, "isoformat"):
             tr_start = tr_start.isoformat()
         start = pandas_to_datetime(tr_start).to_pydatetime()
     else:
@@ -202,7 +213,7 @@ def _time_to_search_dims(time_range):
         tr_end = datetime.datetime.now().strftime("%Y-%m-%d")
     # Attempt conversion to isoformat
     # allows pandas.Period to handle datetime objects
-    if hasattr(tr_end, 'isoformat'):
+    if hasattr(tr_end, "isoformat"):
         tr_end = tr_end.isoformat()
     # get end of period to ensure range is inclusive
 
