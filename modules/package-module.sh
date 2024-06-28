8aca0f8f4c37a64c16c445fd91ff7753021c8972
Jeremy Hooke <jez@stulk.com>
2024-06-28 13:54:02 +1000

    Update packaging for this branch (for now)

--- modules/package-module.sh
+++ modules/package-module.sh
@@ -84,7 +84,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 
 	echo "Installing ard-scene-select"
 
-	installrepo ard-scene-select  master    https://github.com/GeoscienceAustralia/dea-ard-scene-select.git
+	installrepo ard-scene-select  jez/bulk-process  https://github.com/GeoscienceAustralia/dea-ard-scene-select.git
 
 	echo "Writing modulefile"
 	modulefile_dir="${module_dir}/modulefiles/${package_name}"
