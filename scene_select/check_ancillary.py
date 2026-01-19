b25dacc13c9918b0d183549e6c42a79fb41920a0
Peter Mandile <peter.mandile@ga.gov.au>
2026-01-19 06:18:02 +0000

    added support for S3 discovery
    TODO this is no longer compatible with disk, need to fix the prefixes

--- scene_select/check_ancillary.py
+++ scene_select/check_ancillary.py
@@ -8,6 +8,7 @@ try:
     import tables  # This is needed when testing locally
 except ModuleNotFoundError:
     pass
+import os
 import h5py
 import numpy
 import pandas
@@ -21,6 +22,76 @@ DEFAULT_VIIRS_M_PATH = "/g/data/v10/eoancillarydata-2/BRDF/VNP43MA1.001"  # viir
 DEFAULT_USE_VIIRS_AFTER = datetime.datetime(2099, 9, 9)
 WV_DIR = "/g/data/v10/eoancillarydata-2/water_vapour"
 WV_FMT = "pr_wtr.eatm.{year}.h5"
+
+
+USE_S3 = os.environ.get("USE_S3", "true").lower() == "true"
+S3_BUCKET = os.environ.get("S3_BUCKET", "ard-processing-data")
+# S3_BRDF_PREFIX = os.environ.get("S3_PREFIX", "ancillary")
+S3_PREFIX = os.environ.get("S3_PREFIX", "ancillary")
+WATER_VAPOUR_PREFIX = "water_vapour"
+BRDF_PREFIX = "BRDF"
+if USE_S3:
+    import boto3
+    import logging
+
+    logging.getLogger("botocore").setLevel(logging.WARNING)
+    logging.getLogger("s3transfer").setLevel(logging.WARNING)
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+    logging.getLogger("botocore").propagate = True
+    logging.getLogger("s3transfer").propagate = True
+    logging.getLogger("urllib3").propagate = True
+
+    S3_CLIENT = boto3.client("s3")
+
+
+def to_s3_key(base_path: Path, path: Path) -> str:
+    return str(Path(S3_PREFIX) / path.relative_to(base_path))
+
+
+def to_file_path(base_path: Path, s3_key: str) -> Path:
+    return base_path / s3_key.relative_to(S3_PREFIX)
+
+
+def dir_exists(base_path: Path, path: Path) -> bool:
+    """List children of the given path, either locally or in S3, depending on configuration."""
+    print(f"GETTING CHILDREN FOR {base_path} {path}")
+    if USE_S3:
+        s3_path = to_s3_key(base_path, path)
+        print(f"LISTING S3 PATH AT {s3_path}")
+        response = S3_CLIENT.list_objects_v2(Bucket=S3_BUCKET, Prefix=s3_path)
+        ans= response.get("KeyCount", 0) > 0
+        print("RETURNING", ans)
+        return ans
+    else:
+        return path.is_dir()
+
+
+def download_file(base_path: Path, path: Path) -> bool:
+    path.parent.mkdir(parents=True, exist_ok=True)
+    s3_key = to_s3_key(base_path, path)
+    print("DOWNLOADING", s3_key, "to", path)
+    S3_CLIENT.download_file(S3_BUCKET, s3_key, path)
+
+
+def file_exists(base_path: Path, path: Path) -> bool:
+    """Check if the file exists either locally or in S3, depending on configuration."""
+    from botocore.exceptions import ClientError
+
+    if USE_S3:
+        try:
+            s3_key = to_s3_key(base_path, path)
+            print("LOOKING FOR KEY", s3_key)
+            S3_CLIENT.head_object(Bucket=S3_BUCKET, Key=s3_key)
+            return True
+        except ClientError as e:
+            # Object does not exist.
+            if e.response["Error"]["Code"] == "404":
+                return False
+            # For any other error (e.g., 403 Forbidden, 500 Server Error), re-raise the exception
+            else:
+                return path.exists()
+    else:
+        return False
 
 
 def read_h5_table(fid, dataset_name):
@@ -73,12 +144,20 @@ class AncillaryFiles:
 
     @lru_cache(maxsize=32)
     def wv_file_exists(self, a_year):
-        wv_pathname = self.wv_path.joinpath(WV_FMT.format(year=a_year))
+        wv_pathname = self.wv_path.joinpath(WATER_VAPOUR_PREFIX).joinpath(
+            WV_FMT.format(year=a_year)
+        )
+        print("WE ARE LOOKING AT ", wv_pathname)
+        return file_exists(self.wv_path, wv_pathname)
         return wv_pathname.exists()
 
     @lru_cache(maxsize=32)
     def get_wv_index(self, a_year):
-        wv_pathname = self.wv_path.joinpath(WV_FMT.format(year=a_year))
+        wv_pathname = self.wv_path.joinpath(WATER_VAPOUR_PREFIX).joinpath(
+            WV_FMT.format(year=a_year)
+        )
+        print("WE READING THE INDEX AT ", wv_pathname)
+        download_file(self.wv_path, wv_pathname)
         with h5py.File(str(wv_pathname), "r") as fid:
             index = read_h5_table(fid, "INDEX")
         return index
@@ -86,17 +165,18 @@ class AncillaryFiles:
     @lru_cache(maxsize=20000)
     def brdf_day_exists(self, ymd, base_path):
         brdf_day_of_interest = base_path.joinpath(ymd)
-        return brdf_day_of_interest.is_dir()
+        print("WE ARE LOOKING FOR FOLDER ", brdf_day_of_interest)
+        return dir_exists(base_path, brdf_day_of_interest)
 
     def check_modis(self, ymd):
-        if self.brdf_day_exists(ymd, self.brdf_path):
+        if self.brdf_day_exists( "BRDF/MCD43A1.061/" + ymd, self.brdf_path):
             return True, ""
         else:
             return False, f"MODIS BRDF data for {ymd} does not exist."
 
     def check_viirs(self, ymd):
-        if self.brdf_day_exists(ymd, self.viirs_i_path) and self.brdf_day_exists(
-            ymd, self.viirs_m_path
+        if self.brdf_day_exists("BRDF/VNP43IA1.002/" + ymd, self.viirs_i_path) and self.brdf_day_exists(
+            "BRDF/VNP43MA1.002/" + ymd, self.viirs_m_path
         ):
             return True, ""
         else:
